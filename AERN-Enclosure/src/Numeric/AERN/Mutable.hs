988b24d3c01927391ca6ccdde853e3b8f67e864e
Michal Konečný <mikkonecny@googlemail.com>
2010-02-03 15:56:27 +0000

    added a forgotten new module

---
+++ AERN-Enclosure/src/Numeric/AERN/Mutable.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE TypeFamilies #-}
+{-|
+    Module      :  Numeric.AERN.Mutable
+    Description :  a type class for ST mutable structures 
+    Copyright   :  (c) Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+-}
+module Numeric.AERN.Mutable where
+
+import Control.Monad.ST (ST)
+
+class CanBeMutable t where
+    {-| a mutable version of the type t; 
+        the extra parameter is the state of the ST monad run -}
+    type Mutable t :: * -> *
+
+    {-| a thaw operation to make a variable with an initial value -}
+    makeMutable :: t -> ST s (Mutable t s)
+    {-| a read operation, yielding an immutable value -}
+    readMutable :: Mutable t s -> ST s t
+    {-| a write/update operation -}
+    writeMutable :: Mutable t s -> t -> ST s ()
+
+--propWriteRead ::
+--propWriteWriteRead ::
+--propWriteWriteReadConcurrent ::
