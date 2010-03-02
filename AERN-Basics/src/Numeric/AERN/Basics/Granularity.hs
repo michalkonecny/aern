565bbfb5313da9aeba68d14a016048dedcf92810
Michal Konecny <mikkonecny@googlemail.com>
2010-03-02 12:31:26 +0000

    made it possible for each numeric type to have its own granularity type

--- AERN-Basics/src/Numeric/AERN/Basics/Granularity.hs
+++ AERN-Basics/src/Numeric/AERN/Basics/Granularity.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-|
     Module      :  Numeric.AERN.Basics.Granularity
     Description :  finite subsets of continuum sets (eg floats)
@@ -21,72 +23,83 @@ import Prelude hiding (compare, (<=), (>=))
 import Control.Monad.ST (ST)
 import Test.QuickCheck
 
-{-|
-  The bit size of the floating point numbers (or similar)
-  used internally in real number and function approximations.
--}
-type Granularity = Int
-
-class HasGranularity t where 
+class HasGranularity t where
+    {-|
+      A measure used internally to limit the size of the
+      values of t eg when constructing new values from old ones.
+      (Think eg of the mantisa size of a floating point number
+       or the degree limit of a polynomial.)
+    -}
+    type Granularity t
     {-| extract the internal granularity of a value of e -}
-    getGranularity :: t -> Granularity
+    getGranularity :: t -> Granularity t
     
 class (CanBeMutable t, HasGranularity t) => HasGranularityMutable t
     where
     {-| extract the internal granularity of a value stored in an ST variable -}
-    getGranularityMutable :: Mutable t s -> ST s Granularity
+    getGranularityMutable :: Mutable t s -> ST s (Granularity t)
     
-class (HasGranularity t) => (CanSetGranularityOrder t) where
-    setGranularityUp :: Granularity -> t -> t
-    setGranularityDn :: Granularity -> t -> t
-    setMinGranularityUp :: Granularity -> t -> t
-    setMinGranularityDn :: Granularity -> t -> t
+class (HasGranularity t) => (CanSetGranularityRoundedByNumericOrder t) where
+    setGranularityUp :: Granularity t -> t -> t
+    setGranularityDn :: Granularity t -> t -> t
+    setMinGranularityUp :: Granularity t -> t -> t
+    setMinGranularityDn :: Granularity t -> t -> t
 
-propSetGranularityUp :: (Poset t, CanSetGranularityOrder t) => t -> Granularity -> Bool
+propSetGranularityUp ::
+        (Poset t, CanSetGranularityRoundedByNumericOrder t, Eq (Granularity t)) => 
+        t -> Granularity t -> Bool
 propSetGranularityUp e gr =
         (gr == getGranularity e') && (e' >= e)
         where
         e' = setGranularityUp gr e
  
-propSetGranularityDn :: (Poset t, CanSetGranularityOrder t) => t -> Granularity -> Bool
+propSetGranularityDn :: 
+        (Poset t, CanSetGranularityRoundedByNumericOrder t, Eq (Granularity t)) => 
+        t -> Granularity t -> Bool
 propSetGranularityDn e gr =
         (gr == getGranularity e') && (e' <= e)
         where
         e' = setGranularityDn gr e
  
-propSetMinGranularityUp :: (Poset t, CanSetGranularityOrder t) => t -> Granularity -> Bool
+propSetMinGranularityUp :: 
+        (Poset t, CanSetGranularityRoundedByNumericOrder t, Poset (Granularity t)) => 
+        t -> Granularity t -> Bool
 propSetMinGranularityUp e gr =
-        (gr Prelude.<= getGranularity e') && (e' >= e)
+        (gr <= getGranularity e') && (e' >= e)
         where
         e' = setMinGranularityUp gr e
  
-propSetMinGranularityDn :: (Poset t, CanSetGranularityOrder t) => t -> Granularity -> Bool
+propSetMinGranularityDn :: 
+        (Poset t, CanSetGranularityRoundedByNumericOrder t, Poset (Granularity t)) => 
+        t -> Granularity t -> Bool
 propSetMinGranularityDn e gr =
-        (gr Prelude.<= getGranularity e') && (e' <= e)
+        (gr <= getGranularity e') && (e' <= e)
         where
         e' = setMinGranularityDn gr e
  
-class (HasGranularity t) => (CanSetGranularityEnclosure t) where
-    setGranularityOut :: Granularity -> t -> t
-    setGranularityIn :: Granularity -> t -> t
-    setMinGranularityOut :: Granularity -> t -> t
-    setMinGranularityIn :: Granularity -> t -> t
+class (HasGranularity t) => (CanSetGranularityRoundedByRefinementOrder t) where
+    setGranularityOut :: Granularity t -> t -> t
+    setGranularityIn :: Granularity t -> t -> t
+    setMinGranularityOut :: Granularity t -> t -> t
+    setMinGranularityIn :: Granularity t -> t -> t
 
 -- TODO: add analogous properties as for CanSetGranularityOrder
 
-class (CanSetGranularityOrder t, CanBeMutable t) => (CanSetGranularityOrderMutable t) where
-    setGranularityUpMutable :: Granularity -> Mutable t s -> ST s ()
-    setGranularityDnMutable :: Granularity -> Mutable t s -> ST s ()
-    setMinGranularityUpMutable :: Granularity -> Mutable t s -> ST s ()
-    setMinGranularityDnMutable :: Granularity -> Mutable t s -> ST s ()
+class (CanSetGranularityRoundedByNumericOrder t, CanBeMutable t) => 
+        (CanSetGranularityRoundedByNumericOrderMutable t) where
+    setGranularityUpMutable :: Granularity t -> Mutable t s -> ST s ()
+    setGranularityDnMutable :: Granularity t -> Mutable t s -> ST s ()
+    setMinGranularityUpMutable :: Granularity t -> Mutable t s -> ST s ()
+    setMinGranularityDnMutable :: Granularity t -> Mutable t s -> ST s ()
 
 -- TODO: add analogous properties as for CanSetGranularityOrder
 
-class (CanSetGranularityEnclosure t, CanBeMutable t) => (CanSetGranularityEnclosureMutable t) where
-    setGranularityOutMutable :: Granularity -> Mutable t s -> ST s ()
-    setGranularityInMutable :: Granularity -> Mutable t s -> ST s ()
-    setMinGranularityOutMutable :: Granularity -> Mutable t s -> ST s ()
-    setMinGranularityInMutable :: Granularity -> Mutable t s -> ST s ()
+class (CanSetGranularityRoundedByRefinementOrder t, CanBeMutable t) => 
+        (CanSetGranularityRoundedByRefinementOrderMutable t) where
+    setGranularityOutMutable :: Granularity t -> Mutable t s -> ST s ()
+    setGranularityInMutable :: Granularity t -> Mutable t s -> ST s ()
+    setMinGranularityOutMutable :: Granularity t -> Mutable t s -> ST s ()
+    setMinGranularityInMutable :: Granularity t -> Mutable t s -> ST s ()
 
 -- TODO: add analogous properties as for CanSetGranularityOrderMutable
     
