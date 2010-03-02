7200d2b5c3eed5bd473bc1cd38f4787929d63d2e
Michal Konečný <mikkonecny@gmail.com>
2010-03-02 17:08:43 +0000

    Fixed handling of effort in the RoundedLattice class.

--- AERN-Basics/src/Numeric/AERN/Basics/NumericOrder/RoundedLattice.hs
+++ AERN-Basics/src/Numeric/AERN/Basics/NumericOrder/RoundedLattice.hs
@@ -25,10 +25,23 @@ import Numeric.AERN.Basics.Extrema
     A type with directed-rounding lattice operations.
 -}
 class RoundedLattice t where
-    maxUp :: [EffortIndicator] -> t -> t -> t
-    maxDn :: [EffortIndicator] -> t -> t -> t
-    minUp :: [EffortIndicator] -> t -> t -> t
-    minDn :: [EffortIndicator] -> t -> t -> t
+    maxUpEff :: [EffortIndicator] -> t -> t -> t
+    maxDnEff :: [EffortIndicator] -> t -> t -> t
+    minUpEff :: [EffortIndicator] -> t -> t -> t
+    minDnEff :: [EffortIndicator] -> t -> t -> t
+    minmaxDefaultEffort :: t -> [EffortIndicator]
+
+    maxUp :: t -> t -> t
+    maxDn :: t -> t -> t
+    minUp :: t -> t -> t
+    minDn :: t -> t -> t
+    
+    maxUp a b = maxUpEff (minmaxDefaultEffort a) a b 
+    maxDn a b = maxDnEff (minmaxDefaultEffort a) a b 
+    minUp a b = minUpEff (minmaxDefaultEffort a) a b 
+    minDn a b = minDnEff (minmaxDefaultEffort a) a b 
+
+-- properties of RoundedLattice (TODO)
     
 {-|
     A type with directed-rounding lattice operations
@@ -36,13 +49,13 @@ class RoundedLattice t where
 -}
 class (RoundedLattice t, CanBeMutable t) => RoundedLatticeMutable t where
     {-| maxUpMutable e a b c means a := maxUp e b c; a can be the same as b and/or c -}
-    maxUpMutable :: Mutable t s -> Mutable t s -> Mutable t s -> ST s ()
+    maxUpMutable :: [EffortIndicator] -> Mutable t s -> Mutable t s -> Mutable t s -> ST s ()
     {-| maxDnMutable e a b c means a := maxDn e b c; a can be the same as b and/or c -}
-    maxDnMutable :: Mutable t s -> Mutable t s -> Mutable t s -> ST s ()
+    maxDnMutable :: [EffortIndicator] -> Mutable t s -> Mutable t s -> Mutable t s -> ST s ()
     {-| minUpMutable e a b c means a := minUp e b c; a can be the same as b and/or c -}
-    minUpMutable :: Mutable t s -> Mutable t s -> Mutable t s -> ST s ()
+    minUpMutable :: [EffortIndicator] -> Mutable t s -> Mutable t s -> Mutable t s -> ST s ()
     {-| minDnMutable e a b c means a := minDn e b c; a can be the same as b and/or c -}
-    minDnMutable :: Mutable t s -> Mutable t s -> Mutable t s -> ST s ()
+    minDnMutable :: [EffortIndicator] -> Mutable t s -> Mutable t s -> Mutable t s -> ST s ()
 
     -- TODO: add default implementations using read/write
     
