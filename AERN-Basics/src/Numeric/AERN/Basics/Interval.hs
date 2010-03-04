c5fb456559ba047ee58639b5cc91fe38b4d77f18
Michal Konečný <mikkonecny@gmail.com>
2010-03-04 16:56:31 +0000

    the interval class now has a default interval datatype implementation

--- AERN-Basics/src/Numeric/AERN/Basics/Interval.hs
+++ AERN-Basics/src/Numeric/AERN/Basics/Interval.hs
@@ -16,10 +16,27 @@ module Numeric.AERN.Basics.Interval where
     Pairs of endpoints.  A user should not use this type directly
     but use the classes of which this is an instance.
 -}
-class Interval i where
-  type Endpoint i
-  getEndpoints :: i -> (Endpoint i, Endpoint i)
-  fromEndpoints :: (Endpoint i, Endpoint i) -> i
-  mapEndpoints :: (Endpoint i -> Endpoint i) -> (i -> i)
-  mapEndpointPair :: ((Endpoint i, Endpoint i) -> (Endpoint i, Endpoint i)) -> (i -> i)
+class CInterval i where
+    type Endpoint i
+    getEndpoints :: i -> (Endpoint i, Endpoint i)
+    fromEndpoints :: (Endpoint i, Endpoint i) -> i
+    mapEndpoints :: (Endpoint i -> Endpoint i) -> (i -> i)
+    mapEndpointPair :: ((Endpoint i, Endpoint i) -> (Endpoint i, Endpoint i)) -> (i -> i)
+
   
+data Interval e =
+    Interval
+    { 
+        lowEndpoint :: e,
+        highEndpoint :: e
+    }
+   
+instance CInterval (Interval e) where
+    type Endpoint (Interval e) = e
+    getEndpoints (Interval l h) = (l, h)
+    fromEndpoints (l,h) = Interval l h     
+    mapEndpoints f (Interval l h) = Interval (f l) (f h)
+    mapEndpointPair f (Interval l h) = 
+        Interval l2 h2
+        where
+        (l2, h2) = f (l, h)
