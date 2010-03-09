cfda3a7f986baab58093de95728ffd23a47289c5
Michal Konečný <mikkonecny@gmail.com>
2010-03-09 18:29:57 +0000

    added a test executable with a first mini bunch of tests (yes, they all passed!); improved package dependency specification; added dependency on test-framework because it is needed for Double instances

---
+++ AERN-Real/tests/Main.hs
@@ -0,0 +1,22 @@
+{-|
+    Module      :  Main
+    Description :  run all tests defined in the AERN-Real package  
+    Copyright   :  (c) Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+-}
+module Main where
+
+import Test.Framework (defaultMain)
+
+import Numeric.AERN.RealArithmetic.Basis.Double
+
+main = defaultMain tests
+
+tests = 
+    [
+       testsDoubleEq
+    ]
