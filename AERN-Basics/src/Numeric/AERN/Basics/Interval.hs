9ffe61e785145ec9ddc1e6595898de5e8b5a9751
Michal Konecny <mikkonecny@googlemail.com>
2010-03-11 16:57:10 +0000

    random generation of intervals with no guarantee of consistency

--- AERN-Basics/src/Numeric/AERN/Basics/Interval.hs
+++ AERN-Basics/src/Numeric/AERN/Basics/Interval.hs
@@ -54,6 +54,11 @@ instance CInterval (Interval e) where
         where
         (l2, h2) = f (l, h)
 
+-- random generation of intervals with no guarantee of consistency: 
+instance (Arbitrary e) => Arbitrary (Interval e)
+    where
+    arbitrary = arbitrary >>= return . fromEndpoints
+
 {-| type for random generation of consistent intervals -}        
 data ConsistentInterval e = ConsistentInterval (Interval e) deriving (Show)
         
