6c5df0b53dd1470715e55178bdd5219879195671
Michal Konecny <mikkonecny@googlemail.com>
2010-03-18 16:11:21 +0000

    added dummy instances for arbitrary generation of interval tuples

--- AERN-Basics/src/Numeric/AERN/Basics/Interval/NumericOrder.hs
+++ AERN-Basics/src/Numeric/AERN/Basics/Interval/NumericOrder.hs
@@ -42,4 +42,6 @@ instance (NumOrd.HasHighest e) => (NumOrd.HasHighest (Interval e))
     
 instance (NumOrd.HasExtrema e) => (NumOrd.HasExtrema (Interval e))
 
-    
+instance (NumOrd.ArbitraryOrderedTuple e) => NumOrd.ArbitraryOrderedTuple (Interval e) where
+   arbitraryTupleRelatedBy indices constraints =
+       Nothing --TODO
