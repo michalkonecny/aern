47ec03a0fb8a1156d872972223b821ab443f5d5d
Michal Konecny <mikkonecny@googlemail.com>
2010-04-27 18:25:18 +0100

    added tests for various properties of addition, absolute value and multiplication

--- AERN-Real/src/Numeric/AERN/RealArithmetic/NumericOrderRounding/RoundedOps.hs
+++ AERN-Real/src/Numeric/AERN/RealArithmetic/NumericOrderRounding/RoundedOps.hs
@@ -18,8 +18,8 @@
 module Numeric.AERN.RealArithmetic.NumericOrderRounding.RoundedOps 
 (
     RoundedAdd(..), RoundedSubtr(..), testsUpDnAdd,
-    RoundedAbs(..), 
-    RoundedMultiply(..), RoundedDivide(..)
+    RoundedAbs(..), testsUpDnAbs, 
+    RoundedMultiply(..), RoundedDivide(..), testsUpDnMult
 )
 where
 
@@ -87,19 +87,31 @@ propUpDnAddCommutative ::
 propUpDnAddCommutative _ effortDist =
     roundedImprovingCommutative NumOrd.pLeqEff (distanceBetweenEff effortDist) addUpEff addDnEff
        
---propUpDnAddAssociative ::
---    (NumOrd.Comparison t, RoundedAdd t) =>
---    t -> t -> t -> t -> Bool
---propUpDnAddAssociative _ =
---    roundedAssociative (NumOrd.<=?) (+^) (+.)
---       
+propUpDnAddAssociative ::
+    (NumOrd.PartialComparison t, RoundedAdd t, HasZero t,
+     HasDistance t,  
+     NumOrd.Comparison (Distance t), HasZero (Distance t),
+     Show (AddEffortIndicator t),
+     EffortIndicator (AddEffortIndicator t),
+     Show (NumOrd.PartialCompareEffortIndicator t),
+     EffortIndicator (NumOrd.PartialCompareEffortIndicator t)
+     ) =>
+    t ->
+    (DistanceEffortIndicator t) -> 
+    (NumOrd.PartialCompareEffortIndicator (Distance t)) -> 
+    (NumOrd.PartialCompareEffortIndicator t, AddEffortIndicator t) -> 
+    t -> t -> t -> Bool
+propUpDnAddAssociative _ effortDist =
+    roundedImprovingAssociative NumOrd.pLeqEff (distanceBetweenEff effortDist) addUpEff addDnEff
 
 testsUpDnAdd (name, sample) =
     testGroup (name ++ " +. +^") $
         [
             testProperty "0 absorbs" (propUpDnAddZero sample)
         ,
-            testProperty "commutativity" (propUpDnAddCommutative sample)
+            testProperty "commutative" (propUpDnAddCommutative sample)
+        ,
+            testProperty "associative" (propUpDnAddAssociative sample)
         ]
         
 class (RoundedAdd t, Neg t) => RoundedSubtr t where
@@ -148,6 +160,49 @@ absDnUsingCompMax (effortComp, effortMinmax) a =
     where
     max = NumOrd.maxDnEff effortMinmax
 
+propUpDnAbsNegSymmetric ::
+    (NumOrd.PartialComparison t, RoundedAbs t, HasZero t,
+     HasDistance t,  Neg t,
+     NumOrd.Comparison (Distance t), HasZero (Distance t),
+     Show (AbsEffortIndicator t),
+     EffortIndicator (AbsEffortIndicator t),
+     Show (NumOrd.PartialCompareEffortIndicator t),
+     EffortIndicator (NumOrd.PartialCompareEffortIndicator t)
+     ) =>
+    t ->
+    (DistanceEffortIndicator t) -> 
+    (NumOrd.PartialCompareEffortIndicator (Distance t)) -> 
+    (NumOrd.PartialCompareEffortIndicator t, AbsEffortIndicator t) -> 
+    t -> Bool
+propUpDnAbsNegSymmetric _ effortDist =
+    roundedImprovingNegSymmetric NumOrd.pLeqEff (distanceBetweenEff effortDist) absUpEff absDnEff
+
+propUpDnAbsIdempotent ::
+    (NumOrd.PartialComparison t, RoundedAbs t, HasZero t,
+     HasDistance t,
+     NumOrd.Comparison (Distance t), HasZero (Distance t),
+     Show (AbsEffortIndicator t),
+     EffortIndicator (AbsEffortIndicator t),
+     Show (NumOrd.PartialCompareEffortIndicator t),
+     EffortIndicator (NumOrd.PartialCompareEffortIndicator t)
+     ) =>
+    t ->
+    (DistanceEffortIndicator t) -> 
+    (NumOrd.PartialCompareEffortIndicator (Distance t)) -> 
+    (NumOrd.PartialCompareEffortIndicator t, AbsEffortIndicator t) -> 
+    t -> Bool
+propUpDnAbsIdempotent _ effortDist =
+    roundedImprovingIdempotent NumOrd.pLeqEff (distanceBetweenEff effortDist) absUpEff absDnEff
+
+testsUpDnAbs (name, sample) =
+    testGroup (name ++ " up/dn rounded abs") $
+        [
+            testProperty "neg -> no change" (propUpDnAbsNegSymmetric sample)
+        ,
+            testProperty "idempotent" (propUpDnAbsIdempotent sample)
+        ]
+
+
 class RoundedMultiply t where
     type MultEffortIndicator t
     multUpEff :: MultEffortIndicator t -> t -> t -> t
@@ -160,6 +215,93 @@ class RoundedMultiply t where
 
 class (RoundedAdd t, RoundedSubtr t, RoundedMultiply t) => RoundedRRing t
 
+propUpDnMultOne ::
+    (NumOrd.Comparison t, RoundedMultiply t, HasOne t,
+     HasDistance t,  
+     NumOrd.Comparison (Distance t), HasZero (Distance t),
+     Show (MultEffortIndicator t),
+     EffortIndicator (MultEffortIndicator t),
+     Show (NumOrd.PartialCompareEffortIndicator t),
+     EffortIndicator (NumOrd.PartialCompareEffortIndicator t)
+     ) =>
+    t ->
+    (DistanceEffortIndicator t) -> 
+    (NumOrd.PartialCompareEffortIndicator (Distance t)) -> 
+    (NumOrd.PartialCompareEffortIndicator t, MultEffortIndicator t) -> 
+    t -> Bool
+propUpDnMultOne _ effortDist =
+    roundedImprovingUnit one NumOrd.pLeqEff (distanceBetweenEff effortDist) multUpEff multDnEff
+
+propUpDnMultCommutative ::
+    (NumOrd.Comparison t, RoundedMultiply t, HasZero t,
+     HasDistance t,  
+     NumOrd.Comparison (Distance t), HasZero (Distance t),
+     Show (MultEffortIndicator t),
+     EffortIndicator (MultEffortIndicator t),
+     Show (NumOrd.PartialCompareEffortIndicator t),
+     EffortIndicator (NumOrd.PartialCompareEffortIndicator t)
+     ) =>
+    t ->
+    (DistanceEffortIndicator t) -> 
+    (NumOrd.PartialCompareEffortIndicator (Distance t)) -> 
+    (NumOrd.PartialCompareEffortIndicator t, MultEffortIndicator t) -> 
+    t -> t -> Bool
+propUpDnMultCommutative _ effortDist =
+    roundedImprovingCommutative NumOrd.pLeqEff (distanceBetweenEff effortDist) multUpEff multDnEff
+       
+propUpDnMultAssociative ::
+    (NumOrd.PartialComparison t, NumOrd.RoundedLattice t, 
+     RoundedMultiply t, HasZero t,
+     HasDistance t,  
+     NumOrd.Comparison (Distance t), HasZero (Distance t),
+     Show (MultEffortIndicator t),
+     EffortIndicator (MultEffortIndicator t),
+     Show (NumOrd.PartialCompareEffortIndicator t),
+     EffortIndicator (NumOrd.PartialCompareEffortIndicator t)
+     ) =>
+    t ->
+    (DistanceEffortIndicator t) -> 
+    (NumOrd.PartialCompareEffortIndicator (Distance t)) -> 
+    (NumOrd.MinmaxEffortIndicator t) -> 
+    (NumOrd.PartialCompareEffortIndicator t, MultEffortIndicator t) -> 
+    t -> t -> t -> Bool
+propUpDnMultAssociative _ effortDist effortDistComp minmaxEffort initEffort e1 e2 e3 =
+    equalRoundingUpDnImprovement
+        expr1Up expr1Dn expr2Up expr2Dn 
+        NumOrd.pLeqEff (distanceBetweenEff effortDist) effortDistComp initEffort
+    where
+    expr1Up eff =
+        let ?multUpDnEffort = eff in
+        let r1 = e1 *^ (e2 *^ e3) in
+        let r2 = e1 *^ (e2 *. e3) in
+        NumOrd.maxUpEff minmaxEffort r1 r2
+    expr1Dn eff =
+        let ?multUpDnEffort = eff in
+        let r1 = e1 *. (e2 *^ e3) in
+        let r2 = e1 *. (e2 *. e3) in
+        NumOrd.minDnEff minmaxEffort r1 r2
+    expr2Up eff =
+        let ?multUpDnEffort = eff in
+        let r1 = (e1 *^ e2) *^ e3 in
+        let r2 = (e1 *. e2) *^ e3 in
+        NumOrd.maxUpEff minmaxEffort r1 r2
+    expr2Dn eff =
+        let ?multUpDnEffort = eff in
+        let r1 = (e1 *^ e2) *. e3 in
+        let r2 = (e1 *. e2) *. e3 in
+        NumOrd.minDnEff minmaxEffort r1 r2
+    
+testsUpDnMult (name, sample) =
+    testGroup (name ++ " *. *^") $
+        [
+            testProperty "1 absorbs" (propUpDnMultOne sample)
+        ,
+            testProperty "commutative" (propUpDnMultCommutative sample)
+        ,
+            testProperty "associative" (propUpDnMultAssociative sample)
+        ]
+
+
 class RoundedDivide t where
     type DivEffortIndicator t
     divUpEff :: DivEffortIndicator t -> t -> t -> t
@@ -171,59 +313,4 @@ class RoundedDivide t where
     (/.) = divDnEff ?divUpDnEffort
 
 class (RoundedRRing t, RoundedDivide t) => RoundedField t
-
---propUpDnMultOne ::
---    (NumOrd.Comparison t, RoundedMult t, HasOne t) =>
---    t -> t -> Bool
---propUpDnMultOne _ =
---    roundedUnit one (NumOrd.<=?) (*^) (*.)
---
---propUpDnMultCommutative ::
---    (NumOrd.Comparison t, RoundedMult t) =>
---    t -> t -> t -> Bool
---propUpDnMultCommutative _ =
---    roundedCommutative (NumOrd.<=?) (*^) (*.)
---       
---propUpDnMultAssociative ::
---    (NumOrd.Comparison t, RoundedMult t) =>
---    t -> t -> t -> t -> Bool
---propUpDnMultAssociative _ =
---    roundedAssociative (NumOrd.<=?) (*^) (*.)
---
---propUpDnMultAddDistributive ::
---    (NumOrd.Comparison t, RoundedMult t, RoundedAdd t) =>
---    t -> t -> t -> t -> Bool
---propUpDnMultAddDistributive _ =
---    roundedLeftDistributive (NumOrd.<=?) (+^) (*^) (+.) (*.)
---
---testsRoundedAddition ::
---    (NumOrd.Comparison t, RoundedAdd t, HasZero t, 
---     Arbitrary t) =>
---    (String, t) -> Test
---testsRoundedAddition (name, sample) =
---    testGroup (name ++ " rounded addition") $ 
---        [
---         testProperty "absorbs zero" (propUpDnAddZero sample)
---        ,
---         testProperty "commutative" (propUpDnAddCommutative sample)
---        ,
---         testProperty "associative" (propUpDnAddAssociative sample)
---        ]
---
---testsRoundedMultiplication ::
---    (NumOrd.Comparison t, RoundedMult t, RoundedAdd t, HasOne t, 
---     Arbitrary t) =>
---    (String, t) -> Test
---testsRoundedMultiplication (name, sample) =
---    testGroup (name ++ " rounded multiplication") $ 
---        [
---         testProperty "absorbs one" (propUpDnMultOne sample)
---        ,
---         testProperty "commutative" (propUpDnMultCommutative sample)
---        ,
---         testProperty "associative" (propUpDnMultAssociative sample)
---        ,
---         testProperty "distributes over addition" (propUpDnMultAddDistributive sample)
---        ]
-
 
