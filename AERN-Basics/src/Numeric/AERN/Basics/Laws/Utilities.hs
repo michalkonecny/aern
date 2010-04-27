b44e94a57fbc8c627828f80c784d9f14de3a4ddb
Michal Konečný <mikkonecny@gmail.com>
2010-04-27 18:24:45 +0100

    added a few utility type synonyms for unary operations

--- AERN-Basics/src/Numeric/AERN/Basics/Laws/Utilities.hs
+++ AERN-Basics/src/Numeric/AERN/Basics/Laws/Utilities.hs
@@ -22,9 +22,11 @@ type Expr1Eff ei t = ei -> t -> t
 type Expr2Eff ei t = ei -> t -> t -> t
 type Expr3Eff ei t = ei -> t -> t -> t -> t
 
+type UnaryOp t = t -> t
 type Op t = t -> t -> t
 type PartOp t = t -> t -> Maybe t
 
+type UnaryOpEff ei t = ei -> t -> t
 type OpEff ei t = ei -> t -> t -> t
 type PartOpEff ei t = ei -> t -> t -> Maybe t
 
@@ -39,6 +41,7 @@ type Expr2Op1 t = (Op t) -> (Op t) -> Expr1 t
 type Expr2Op2 t = (Op t) -> (Op t) -> Expr2 t
 type Expr2Op3 t = (Op t) -> (Op t) -> Expr3 t
 
+type Expr1UnaryOp1Eff ei t = (UnaryOpEff ei t) -> Expr1Eff ei t
 type Expr1Op1Eff ei t = (OpEff ei t) -> Expr1Eff ei t
 type Expr1Op2Eff ei t = (OpEff ei t) -> Expr2Eff ei t
 type Expr1Op3Eff ei t = (OpEff ei t) -> Expr3Eff ei t
