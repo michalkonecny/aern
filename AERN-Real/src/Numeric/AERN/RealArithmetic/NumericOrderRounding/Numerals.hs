0de947621bcf868346a021d822641cef645fd36b
Michal Konečný <mikkonecny@gmail.com>
2010-05-12 08:45:24 +0100

    fixed type class contexts for integer conversion tests and added analogous Double conversion tests

--- AERN-Real/src/Numeric/AERN/RealArithmetic/NumericOrderRounding/Numerals.hs
+++ AERN-Real/src/Numeric/AERN/RealArithmetic/NumericOrderRounding/Numerals.hs
@@ -24,6 +24,8 @@ import Numeric.AERN.Basics.Effort
 import Numeric.AERN.Basics.PartialOrdering
 import qualified Numeric.AERN.Basics.NumericOrder as NumOrd
 
+import Numeric.AERN.Misc.Maybe
+
 import Data.Ratio
 
 import Test.QuickCheck
@@ -37,19 +39,19 @@ class FromInteger t where
     fromIntegerDnEff :: FromIntegerEffortIndicator t -> Integer -> t
 
 propFromIntegerMonotone ::
-    (FromInteger t, NumOrd.Comparison t) =>
+    (FromInteger t, NumOrd.PartialComparison t) =>
     t ->
-    (FromIntegerEffortIndicator t) ->  
+    (FromIntegerEffortIndicator t, NumOrd.PartialCompareEffortIndicator t) ->  
     Integer -> Integer -> Bool
-propFromIntegerMonotone sample effort i1 i2 
-    | i1 > i2 = a1Up NumOrd.> a2Dn
-    | i1 < i2 = a1Dn NumOrd.< a2Up
+propFromIntegerMonotone sample (effortFrom, effortComp) i1 i2 
+    | i1 > i2 = trueOrNothing $ let ?pCompareEffort = effortComp in a1Up NumOrd.>? a2Dn
+    | i1 < i2 = trueOrNothing $ let ?pCompareEffort = effortComp in a1Dn NumOrd.<? a2Up
     | otherwise = True
     where
-    a1Dn = fromIntegerDnEff effort i1 
-    a1Up = fromIntegerUpEff effort i1 
-    a2Dn = fromIntegerDnEff effort i2 
-    a2Up = fromIntegerUpEff effort i2 
+    a1Dn = fromIntegerDnEff effortFrom i1 
+    a1Up = fromIntegerUpEff effortFrom i1 
+    a2Dn = fromIntegerDnEff effortFrom i2 
+    a2Up = fromIntegerUpEff effortFrom i2 
     _ = [sample, a1Dn, a1Up]
     
 class ToInteger t where
@@ -59,7 +61,7 @@ class ToInteger t where
     toIntegerDnEff :: ToIntegerEffortIndicator t -> t -> Integer
 
 propToIntegerMonotone ::
-    (ToInteger t, NumOrd.Comparison t) =>
+    (ToInteger t, NumOrd.PartialComparison t) =>
     t -> 
     (NumOrd.PartialCompareEffortIndicator t, ToIntegerEffortIndicator t) -> 
     t -> t -> Bool
@@ -78,7 +80,7 @@ propToIntegerMonotone sample (effortComp, effortConv) a1 a2 =
     i2Up = toIntegerUpEff effortConv a2 
     
 propToFromInteger ::
-    (ToInteger t, FromInteger t, NumOrd.Comparison t) =>
+    (ToInteger t, FromInteger t, NumOrd.PartialComparison t) =>
     t -> 
     (NumOrd.PartialCompareEffortIndicator t, 
      FromIntegerEffortIndicator t, 
@@ -110,11 +112,74 @@ class FromDouble t where
     fromDoubleDnEff :: FromDoubleEffortIndicator t -> Double -> t
     fromDoubleDefaultEffort :: t -> FromDoubleEffortIndicator t 
 
+propFromDoubleMonotone ::
+    (FromDouble t, NumOrd.PartialComparison t) =>
+    t ->
+    (FromDoubleEffortIndicator t, NumOrd.PartialCompareEffortIndicator t) ->  
+    Double -> Double -> Bool
+propFromDoubleMonotone sample (effortFrom, effortComp) i1 i2 
+    | i1 > i2 = trueOrNothing $ let ?pCompareEffort = effortComp in a1Up NumOrd.>? a2Dn
+    | i1 < i2 = trueOrNothing $ let ?pCompareEffort = effortComp in a1Dn NumOrd.<? a2Up
+    | otherwise = True
+    where
+    a1Dn = fromDoubleDnEff effortFrom i1 
+    a1Up = fromDoubleUpEff effortFrom i1 
+    a2Dn = fromDoubleDnEff effortFrom i2 
+    a2Up = fromDoubleUpEff effortFrom i2 
+    _ = [sample, a1Dn, a1Up]
+
 class ToDouble t where
     type ToDoubleEffortIndicator t
     toDoubleUpEff :: ToDoubleEffortIndicator t -> t -> Double
     toDoubleDnEff :: ToDoubleEffortIndicator t -> t -> Double
     toDoubleDefaultEffort :: t -> ToDoubleEffortIndicator t 
+
+propToDoubleMonotone ::
+    (ToDouble t, NumOrd.PartialComparison t) =>
+    t -> 
+    (NumOrd.PartialCompareEffortIndicator t, ToDoubleEffortIndicator t) -> 
+    t -> t -> Bool
+propToDoubleMonotone sample (effortComp, effortConv) a1 a2 =
+    case NumOrd.pCompareEff effortComp a1 a2 of
+        Just LT -> i1Dn <= i2Up
+        Just LEE -> i1Dn <= i2Up
+        Just GT -> i1Up >= i2Dn
+        Just GEE -> i1Up >= i2Dn
+        Just EQ -> i1Dn <= i1Up
+        _ -> True
+    where
+    i1Dn = toDoubleDnEff effortConv a1 
+    i1Up = toDoubleUpEff effortConv a1 
+    i2Dn = toDoubleDnEff effortConv a2 
+    i2Up = toDoubleUpEff effortConv a2 
+    
+propToFromDouble ::
+    (ToDouble t, FromDouble t, NumOrd.PartialComparison t) =>
+    t -> 
+    (NumOrd.PartialCompareEffortIndicator t, 
+     FromDoubleEffortIndicator t, 
+     ToDoubleEffortIndicator t) ->
+    t -> Bool
+propToFromDouble _ (effortComp, effortFrom, effortTo) a =
+    let ?pCompareEffort = effortComp in
+    case (aDn NumOrd.<=? a, a NumOrd.<=? aUp) of
+       (Just False, _) -> False
+       (_, Just False) -> False
+       _ -> True
+    where
+    aDn = fromDoubleDnEff effortFrom $ toDoubleDnEff effortTo a 
+    aUp = fromDoubleUpEff effortFrom $ toDoubleUpEff effortTo a 
+    
+testsFromToDouble (name, sample) =
+    testGroup (name ++ " Double conversions") $
+        [
+            testProperty "from Double monotone" (propFromDoubleMonotone sample)
+        ,
+            testProperty "to Double monotone" (propToDoubleMonotone sample)
+        ,
+            testProperty "round trip conversion" (propToFromDouble sample)
+        ]
+    
 
 --class FromRatio t where
 --    fromRatioUpEff :: EffortIndicator -> Ratio Integer -> t
