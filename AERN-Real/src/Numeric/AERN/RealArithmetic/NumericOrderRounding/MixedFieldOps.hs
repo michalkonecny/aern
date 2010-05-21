a4799e72f106d40cb5f4d6559c3af8b43ece7590
Michal Konečný <mikkonecny@gmail.com>
2010-05-22 00:30:45 +0100

    tools for instances of mixed field ops via conversion

--- AERN-Real/src/Numeric/AERN/RealArithmetic/NumericOrderRounding/MixedFieldOps.hs
+++ AERN-Real/src/Numeric/AERN/RealArithmetic/NumericOrderRounding/MixedFieldOps.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-|
-    Module      :  Numeric.AERN.NumericOrderRounding.MixedFieldOps
+    Module      :  Numeric.AERN.RealArithmetic.NumericOrderRounding.MixedFieldOps
     Description :  rounded basic arithmetic operations mixing 2 types
     Copyright   :  (c) Michal Konecny
     License     :  BSD3
@@ -17,21 +17,166 @@
 -}
 module Numeric.AERN.RealArithmetic.NumericOrderRounding.MixedFieldOps where
 
+import Numeric.AERN.RealArithmetic.NumericOrderRounding.FieldOps
+import Numeric.AERN.RealArithmetic.NumericOrderRounding.Conversion
+import Numeric.AERN.RealArithmetic.ExactOps
+
+import Numeric.AERN.Basics.Effort
+import Numeric.AERN.RealArithmetic.Laws
+import Numeric.AERN.RealArithmetic.Measures
+import qualified Numeric.AERN.Basics.NumericOrder as NumOrd
+
+import Test.QuickCheck
+import Test.Framework (testGroup, Test)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
 class RoundedMixedAdd s t where
     type MixedAddEffortIndicator s t
+    mixedAddDefaultEffort :: s -> t -> MixedAddEffortIndicator s t
     mixedAddUpEff :: MixedAddEffortIndicator s t -> s -> t -> t
     mixedAddDnEff :: MixedAddEffortIndicator s t -> s -> t -> t
-    mixedAddDefaultEffort :: s -> t -> MixedAddEffortIndicator s t
+
+{- tools to easily make a RoundedMixedAdd instance 
+   via the composition of conversion and homogeneous addition -}
+
+type MixedAddEffortIndicatorByConversion s t =
+        (AddEffortIndicator t, ConvertEffortIndicator s t)
+
+mixedAddDefaultEffortByConversion n d = 
+        (addDefaultEffort d, convertDefaultEffort n d)
+
+mixedAddUpEffByConversion ::
+    (Convertible t1 t2, RoundedAdd t2) =>
+    (AddEffortIndicator t2, ConvertEffortIndicator t1 t2) ->
+    t1 -> t2 -> t2
+mixedAddUpEffByConversion (effAdd, effConv) a b = 
+    addUpEff effAdd (convertUpEff effConv a) b
+
+mixedAddDnEffByConversion ::
+    (Convertible t1 t2, RoundedAdd t2) =>
+    (AddEffortIndicator t2, ConvertEffortIndicator t1 t2) ->
+    t1 -> t2 -> t2
+mixedAddDnEffByConversion (effAdd, effConv) a b = 
+    addDnEff effAdd (convertDnEff effConv a) b
+
+{- properties of mixed addition -}
+
+propMixedAddEqualsConvert ::
+    (NumOrd.PartialComparison t2, Convertible t1 t2,
+     RoundedMixedAdd t1 t2, RoundedAdd t2,
+     HasDistance t2,  Show (Distance t2),  
+     NumOrd.PartialComparison (Distance t2), 
+     HasInfinities (Distance t2), HasZero (Distance t2),
+     Show (MixedAddEffortIndicator t1 t2),
+     EffortIndicator (MixedAddEffortIndicator t1 t2),
+     Show (ConvertEffortIndicator t1 t2),
+     EffortIndicator (ConvertEffortIndicator t1 t2),
+     Show (AddEffortIndicator t2),
+     EffortIndicator (AddEffortIndicator t2),
+     Show (NumOrd.PartialCompareEffortIndicator t2),
+     EffortIndicator (NumOrd.PartialCompareEffortIndicator t2)
+     ) =>
+    t1 -> t2 ->
+    (DistanceEffortIndicator t2) -> 
+    (NumOrd.PartialCompareEffortIndicator (Distance t2)) -> 
+    (NumOrd.PartialCompareEffortIndicator t2,
+     (MixedAddEffortIndicator t1 t2,      
+      AddEffortIndicator t2,
+      ConvertEffortIndicator t1 t2)) -> 
+    t1 -> t2 -> Bool
+propMixedAddEqualsConvert sample1 sample2 effortDist effortDistComp initEffort e1 e2 =
+    equalRoundingUpDnImprovement
+        expr1Up expr1Dn expr2Up expr2Dn 
+        NumOrd.pLeqEff (distanceBetweenEff effortDist) effortDistComp initEffort
+    where
+    expr1Up (effMAdd,_,_) =
+        let (|+^) = mixedAddUpEff effMAdd in e1 |+^ e2
+    expr1Dn (effMAdd,_,_) =
+        let (|+.) = mixedAddDnEff effMAdd in e1 |+. e2
+    expr2Up (_,effAdd,effConv) =
+        let (+^) = addUpEff effAdd in (convertUpEff effConv e1) +^ e2
+    expr2Dn (_,effAdd,effConv) =
+        let (+.) = addDnEff effAdd in (convertDnEff effConv e1) +. e2
 
 class RoundedMixedMultiply s t where
     type MixedMultEffortIndicator s t
+    mixedMultDefaultEffort :: s -> t -> MixedMultEffortIndicator s t
     mixedMultUpEff :: MixedMultEffortIndicator s t -> s -> t -> t
     mixedMultDnEff :: MixedMultEffortIndicator s t -> s -> t -> t
-    mixedMultDefaultEffort :: s -> t -> MixedMultEffortIndicator s t
+
+{- tools to easily make a RoundedMixedMultiply instance 
+   via the composition of conversion and homogeneous addition -}
+
+type MixedMultEffortIndicatorByConversion s t =
+        (MultEffortIndicator t, 
+         ConvertEffortIndicator s t,
+         NumOrd.MinmaxEffortIndicator t)
+
+mixedMultDefaultEffortByConversion n d = 
+        (addDefaultEffort d, 
+         convertDefaultEffort n d,
+         NumOrd.minmaxDefaultEffort d)
+
+mixedMultUpEffByConversion ::
+    (Convertible t1 t2, RoundedMultiply t2, NumOrd.RoundedLattice t2) =>
+    (MultEffortIndicator t2, 
+     ConvertEffortIndicator t1 t2,
+     NumOrd.MinmaxEffortIndicator t2) ->
+    t1 -> t2 -> t2
+mixedMultUpEffByConversion (effMult, effConv, effMinmax) a b =
+    NumOrd.maxUpEff effMinmax  
+    (multUpEff effMult (convertDnEff effConv a) b)
+    (multUpEff effMult (convertUpEff effConv a) b)
+
+mixedMultDnEffByConversion ::
+    (Convertible t1 t2, RoundedMultiply t2, NumOrd.RoundedLattice t2) =>
+    (MultEffortIndicator t2, 
+     ConvertEffortIndicator t1 t2,
+     NumOrd.MinmaxEffortIndicator t2) ->
+    t1 -> t2 -> t2
+mixedMultDnEffByConversion (effMult, effConv, effMinmax) a b = 
+    NumOrd.maxDnEff effMinmax  
+    (multDnEff effMult (convertDnEff effConv a) b)
+    (multDnEff effMult (convertUpEff effConv a) b)
 
 class RoundedMixedDivide s t where
     type MixedDivEffortIndicator s t
+    mixedDivDefaultEffort :: s -> t -> MixedDivEffortIndicator s t
     mixedDivUpEff :: MixedDivEffortIndicator s t -> s -> t -> t
     mixedDivDnEff :: MixedDivEffortIndicator s t -> s -> t -> t
-    mixedDivDefaultEffort :: s -> t -> MixedDivEffortIndicator s t
+
+{- tools to easily make a RoundedMixedDivide instance 
+   via the composition of conversion and homogeneous addition -}
+
+type MixedDivEffortIndicatorByConversion s t =
+        (DivEffortIndicator t, 
+         ConvertEffortIndicator s t,
+         NumOrd.MinmaxEffortIndicator t)
+
+mixedDivDefaultEffortByConversion n d = 
+        (addDefaultEffort d, 
+         convertDefaultEffort n d,
+         NumOrd.minmaxDefaultEffort d)
+
+mixedDivUpEffByConversion ::
+    (Convertible t1 t2, RoundedDivide t2, NumOrd.RoundedLattice t2) =>
+    (DivEffortIndicator t2, 
+     ConvertEffortIndicator t1 t2,
+     NumOrd.MinmaxEffortIndicator t2) ->
+    t1 -> t2 -> t2
+mixedDivUpEffByConversion (effDiv, effConv, effMinmax) a b =
+    NumOrd.maxUpEff effMinmax  
+    (divUpEff effDiv (convertDnEff effConv a) b)
+    (divUpEff effDiv (convertUpEff effConv a) b)
+
+mixedDivDnEffByConversion ::
+    (Convertible t1 t2, RoundedDivide t2, NumOrd.RoundedLattice t2) =>
+    (DivEffortIndicator t2, 
+     ConvertEffortIndicator t1 t2,
+     NumOrd.MinmaxEffortIndicator t2) ->
+    t1 -> t2 -> t2
+mixedDivDnEffByConversion (effDiv, effConv, effMinmax) a b = 
+    NumOrd.maxDnEff effMinmax  
+    (divDnEff effDiv (convertDnEff effConv a) b)
+    (divDnEff effDiv (convertUpEff effConv a) b)
     
