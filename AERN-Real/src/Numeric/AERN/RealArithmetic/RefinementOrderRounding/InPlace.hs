37f4c82963c2e1a6335964a216b530d0c76f6147
Michal Konecny <mikkonecny@googlemail.com>
2010-08-19 14:09:36 +0100

    completed declarations of in-place operations

---
+++ AERN-Real/src/Numeric/AERN/RealArithmetic/RefinementOrderRounding/InPlace.hs
@@ -0,0 +1,27 @@
+{-|
+    Module      :  Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace
+    Description :  common arithmetical operations rounded up/down  
+    Copyright   :  (c) Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+    
+    In-place versions of common arithmetical operations rounded in/out.
+    
+    This module is meant to be imported qualified.
+    It is recommended to use the prefix ArithUpDnInPlace.
+-}
+module Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace
+(
+    module Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace.FieldOps,
+    module Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace.MixedFieldOps,
+    module Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace.Elementary
+)
+where
+
+import Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace.FieldOps
+import Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace.MixedFieldOps
+import Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace.Elementary
+
---
+++ AERN-Real/src/Numeric/AERN/RealArithmetic/RefinementOrderRounding/InPlace/Elementary.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-|
+    Module      :  Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace.Elementary
+    Description :  support for various common elementary functions
+    Copyright   :  (c) Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+    
+    Support for various common elementary functions.
+    
+    This module is hidden and reexported via its parent RefinementOrderRounding.InPlace. 
+-}
+
+module Numeric.AERN.RealArithmetic.RefinementOrderRounding.InPlace.Elementary where
+
+import Numeric.AERN.RealArithmetic.RefinementOrderRounding.Elementary
+
+import Numeric.AERN.RealArithmetic.ExactOps
+import Numeric.AERN.RealArithmetic.RefinementOrderRounding.FieldOps
+
+import Numeric.AERN.Basics.Effort
+import Numeric.AERN.Basics.Mutable
+import Numeric.AERN.RealArithmetic.Laws
+import Numeric.AERN.RealArithmetic.Measures
+import qualified Numeric.AERN.Basics.NumericOrder as NumOrd
+import qualified Numeric.AERN.Basics.RefinementOrder as RefOrd
+
+import Test.QuickCheck
+import Test.Framework (testGroup, Test)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+class (RoundedExponentiation t, CanBeMutable t) => RoundedExponentiationInPlace t where
+    expInInPlaceEff :: t -> OpMutable1Eff (ExpEffortIndicator t) t s
+    expOutInPlaceEff :: t -> OpMutable1Eff (ExpEffortIndicator t) t s
+    expInInPlaceEff sample =
+        pureToMutable1Eff sample expInEff
+    expOutInPlaceEff sample =
+        pureToMutable1Eff sample expOutEff
+
+propInOutExpInPlace ::
+    (RefOrd.PartialComparison t, RoundedExponentiationInPlace t, Neg t,
+     Show t,
+     HasDistance t,  Show (Distance t),  
+     NumOrd.PartialComparison (Distance t), HasInfinities (Distance t), HasZero (Distance t),
+     Show (ExpEffortIndicator t),
+     EffortIndicator (ExpEffortIndicator t),
+     Show (DistanceEffortIndicator t),
+     EffortIndicator (DistanceEffortIndicator t),
+     Show (RefOrd.PartialCompareEffortIndicator t),
+     EffortIndicator (RefOrd.PartialCompareEffortIndicator t)
+     ) =>
+    t ->
+    (NumOrd.PartialCompareEffortIndicator (Distance t)) -> 
+    (DistanceEffortIndicator t, 
+     RefOrd.PartialCompareEffortIndicator t, 
+     ExpEffortIndicator t) -> 
+    t -> Bool
+propInOutExpInPlace sample effortDistComp initEffort e1 =
+    equalRoundingUpDnImprovement
+        expr1In expr1Out expr2In expr2Out 
+        RefOrd.pLeqEff distanceBetweenEff effortDistComp initEffort
+    where
+    expInEffViaInPlace = mutable1EffToPure (expInInPlaceEff sample)
+    expOutEffViaInPlace = mutable1EffToPure (expOutInPlaceEff sample)
+    expr1In eff = expInEff eff e1
+    expr1Out eff = expOutEff eff e1
+    expr2In eff = expInEffViaInPlace eff e1
+    expr2Out eff = expOutEffViaInPlace eff e1
+
+testsInOutExpInPlace (name, sample) =
+    testGroup (name ++ " in place exp") $
+        [
+            testProperty "matches pure" (propInOutExpInPlace sample)
+        ]
