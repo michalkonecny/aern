27c1c796c15031b0db738402d62ee9bd300ed1ba
Michal Konečný <mikkonecny@googlemail.com>
2010-09-01 10:51:30 +0100

    Tidying up package dependencies.

--- AERN-Real-MPFR/src/Numeric/AERN/RealArithmetic/Basis/MPFR/NumericOrder.hs
+++ AERN-Real-MPFR/src/Numeric/AERN/RealArithmetic/Basis/MPFR/NumericOrder.hs
@@ -38,10 +38,6 @@ import Numeric.AERN.Misc.List
 import Numeric.AERN.Misc.Debug
 
 import Data.Maybe
-import qualified Data.Map as Map
---import qualified Data.Set as Set
-
-import Data.Convertible
 
 import System.IO.Unsafe
 
