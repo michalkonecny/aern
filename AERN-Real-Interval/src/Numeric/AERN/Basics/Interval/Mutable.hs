f5bc25c6c2e9ff65e7c70e47ebe9ec09b8268b02
Michal Konecny <mikkonecny@googlemail.com>
2010-10-22 22:10:24 +0100

    added a mutable Interval type

---
+++ AERN-Real-Interval/src/Numeric/AERN/Basics/Interval/Mutable.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE TypeFamilies #-}
+{-|
+    Module      :  Numeric.AERN.Basics.Interval.Mutable
+    Description :  mutable version of Interval
+    Copyright   :  (c) Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+
+    Mutable version of Interval.
+    
+    This is a private module reexported publicly via its parent.
+-}
+
+module Numeric.AERN.Basics.Interval.Mutable where
+
+import Numeric.AERN.Basics.Interval.Basics
+import Numeric.AERN.Basics.Mutable
+
+newtype MInterval e s = MInterval { unMInterval :: Interval (e s) }
+
+instance (CanBeMutable e) => CanBeMutable (Interval e) where
+    type Mutable (Interval e) = MInterval (Mutable e)
+    makeMutable (Interval l h) = 
+        do
+        lM <- makeMutable l
+        hM <- makeMutable h
+        return $ MInterval $ Interval lM hM
+    unsafeMakeMutable (Interval l h) = 
+        do
+        lM <- unsafeMakeMutable l
+        hM <- unsafeMakeMutable h
+        return $ MInterval $ Interval lM hM
+    writeMutable (MInterval (Interval lM hM)) (Interval l h) =
+        do
+        writeMutable lM l
+        writeMutable hM h
+    unsafeWriteMutable (MInterval (Interval lM hM)) (Interval l h) =
+        do
+        unsafeWriteMutable lM l
+        unsafeWriteMutable hM h
+    readMutable (MInterval (Interval lM hM)) =
+        do
+        l <- readMutable lM 
+        h <- readMutable hM 
+        return $ Interval l h
+    unsafeReadMutable (MInterval (Interval lM hM)) =
+        do
+        l <- unsafeReadMutable lM 
+        h <- unsafeReadMutable hM 
+        return $ Interval l h
