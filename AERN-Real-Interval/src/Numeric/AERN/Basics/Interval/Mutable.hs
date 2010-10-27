963ba955a56884d56198ec13462aa35121dd9aee
Michal Konecny <mikkonecny@googlemail.com>
2010-10-27 10:42:45 +0100

    change mutable interval type to remove unnecessary indirection

--- AERN-Real-Interval/src/Numeric/AERN/Basics/Interval/Mutable.hs
+++ AERN-Real-Interval/src/Numeric/AERN/Basics/Interval/Mutable.hs
@@ -19,7 +19,8 @@ module Numeric.AERN.Basics.Interval.Mutable where
 import Numeric.AERN.Basics.Interval.Basics
 import Numeric.AERN.Basics.Mutable
 
-newtype MInterval e s = MInterval { unMInterval :: Interval (e s) }
+data MInterval e s = 
+    MInterval { left :: e s, right :: e s }
 
 instance (CanBeMutable e) => CanBeMutable (Interval e) where
     type Mutable (Interval e) = MInterval (Mutable e)
@@ -27,26 +28,26 @@ instance (CanBeMutable e) => CanBeMutable (Interval e) where
         do
         lM <- makeMutable l
         hM <- makeMutable h
-        return $ MInterval $ Interval lM hM
+        return $ MInterval lM hM
     unsafeMakeMutable (Interval l h) = 
         do
         lM <- unsafeMakeMutable l
         hM <- unsafeMakeMutable h
-        return $ MInterval $ Interval lM hM
-    writeMutable (MInterval (Interval lM hM)) (Interval l h) =
+        return $ MInterval lM hM
+    writeMutable (MInterval lM hM) (Interval l h) =
         do
         writeMutable lM l
         writeMutable hM h
-    unsafeWriteMutable (MInterval (Interval lM hM)) (Interval l h) =
+    unsafeWriteMutable (MInterval lM hM) (Interval l h) =
         do
         unsafeWriteMutable lM l
         unsafeWriteMutable hM h
-    readMutable (MInterval (Interval lM hM)) =
+    readMutable (MInterval lM hM) =
         do
         l <- readMutable lM 
         h <- readMutable hM 
         return $ Interval l h
-    unsafeReadMutable (MInterval (Interval lM hM)) =
+    unsafeReadMutable (MInterval lM hM) =
         do
         l <- unsafeReadMutable lM 
         h <- unsafeReadMutable hM 
