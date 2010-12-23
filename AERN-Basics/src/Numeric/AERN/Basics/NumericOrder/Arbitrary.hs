86f1dbae5be49dba7e5c1fa40be774fc8498e978
Michal Konečný <mikkonecny@gmail.com>
2010-12-23 13:26:02 +0000

    tuned arbitrary generation of Double and Rational within specified areas

--- AERN-Basics/src/Numeric/AERN/Basics/NumericOrder/Arbitrary.hs
+++ AERN-Basics/src/Numeric/AERN/Basics/NumericOrder/Arbitrary.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE TypeFamilies #-}
 {-|
     Module      :  Numeric.AERN.Basics.NumericOrder.Arbitrary
     Description :  random generation of tuples with various relation constraints  
@@ -19,11 +20,16 @@ import Prelude hiding (EQ, LT, GT)
 
 import Numeric.AERN.Basics.PartialOrdering
 
+import Numeric.AERN.Misc.Debug
+
 import Data.Maybe
+import Data.Ratio
 import qualified Data.Map as Map 
 import qualified Data.Set as Set 
 
+
 import Test.QuickCheck
+import System.Random
 import Test.Framework (testGroup, Test)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Numeric.AERN.Misc.QuickCheck
@@ -42,6 +48,21 @@ import System.IO.Unsafe
     where such pairs are rare.
 -}
 class ArbitraryOrderedTuple t where
+    {-| a type of meaningful constraints to place on generation of arbitrary values -}
+    type Area t
+    {-| a special area that puts no constaints on the values -}
+    areaWhole :: t -> Area t
+    {-| generator of tuples that satisfy the given relation requirements
+        and area restriction, 
+        nothing if in this structure there are no tuples satisfying these requirements -}
+    arbitraryTupleInAreaRelatedBy ::
+        (Ord ix, Show ix) =>
+        t {-^ sample for type checking -} ->
+        (Area t) -> 
+        [ix] {-^ how many elements should be generated and with what names -} -> 
+        [((ix, ix),[PartialOrdering])]
+           {-^ required orderings for some pairs of elements -} -> 
+        Maybe (Gen [t]) {-^ generator for tuples if the requirements make sense -}   
     {-| generator of tuples that satisfy the given relation requirements, 
         nothing if in this structure there are no tuples satisfying these requirements -}
     arbitraryTupleRelatedBy ::
@@ -51,38 +72,216 @@ class ArbitraryOrderedTuple t where
            {-^ required orderings for some pairs of elements -} -> 
         Maybe (Gen [t]) {-^ generator for tuples if the requirements make sense -}   
 
+data AreaWholeOnly t =
+    AreaWholeOnly 
+    {
+        areaWholeSpecialValues :: [t]
+    }
+
+arbitraryWhole ::
+    (Arbitrary t) =>
+    AreaWholeOnly t ->
+    Gen t
+arbitraryWhole (AreaWholeOnly []) = incrSize arbitrary
+arbitraryWhole (AreaWholeOnly specialValues) =
+    incrSize $ -- at size 0 we get only 0s...
+    do
+    useSpecial <- elements [False, True, False, False] 
+                        -- 1 in 4 values should be special
+    case useSpecial of
+        True -> elements specialValues
+        False -> arbitrary
+
+data AreaLinear t = 
+    AreaLinear
+    {
+        areaLinLowerBound :: Maybe t,
+        areaLinLowerBoundStrict :: Bool,
+        areaLinUpperBound :: Maybe t,
+        areaLinUpperBoundStrict :: Bool,
+        areaLinSpecialValues :: [t]
+    } 
+
+areaLinearWhole :: [t] -> AreaLinear t
+areaLinearWhole = AreaLinear Nothing True Nothing True 
+
+arbitraryLinear ::
+    (Arbitrary t) =>
+    (t,t) {-^ least and greatest element -} -> 
+    (t -> t) {-^ successor function -} ->
+    (t -> t) {-^ predecessor function -} ->
+    ((t,t) -> Gen t) {-^ choose function -} -> 
+    AreaLinear t ->
+    Gen t
+arbitraryLinear (least, greatest) succ pred choose 
+        (AreaLinear mlb lbStrict mub ubStrict specialValues) =
+    incrSize $ -- at size 0 we get only 0s...
+    do
+    useSpecial <-
+        case specialValues of
+            [] -> return False
+            _ -> elements [False, True, False, False] 
+                 -- 1 in 4 values should be special
+    case useSpecial of
+        True -> elements specialValues
+        False -> 
+            case (mlb, mub) of 
+                (Nothing, Nothing) -> arbitrary
+                _ -> choose (lb, ub) 
+    where
+    lb = 
+        case (mlb, lbStrict) of
+            (Nothing, _) -> least
+            (Just lb, True) -> lb
+            (Just lb, False) -> succ lb
+    ub = 
+        case (mub, ubStrict) of
+            (Nothing, _) -> greatest
+            (Just ub, True) -> ub
+            (Just ub, False) -> pred ub
+
+
 instance ArbitraryOrderedTuple Int where
-    arbitraryTupleRelatedBy = linearArbitraryTupleRelatedBy (incrSize arbitrary)
+    type (Area Int) = AreaWholeOnly Int
+    areaWhole _ = AreaWholeOnly [-1,0,1]
+    arbitraryTupleInAreaRelatedBy sample area = 
+        linearArbitraryTupleRelatedBy $ arbitraryWhole area
+    arbitraryTupleRelatedBy =
+        linearArbitraryTupleRelatedBy $ incrSize arbitrary
 
 instance ArbitraryOrderedTuple Integer where
-    arbitraryTupleRelatedBy = linearArbitraryTupleRelatedBy (incrSize arbitrary)
+    type (Area Integer) = AreaWholeOnly Integer
+    areaWhole _ = AreaWholeOnly [-1,0,1]
+    arbitraryTupleInAreaRelatedBy sample area = 
+        linearArbitraryTupleRelatedBy $ arbitraryWhole area
+    arbitraryTupleRelatedBy =
+        linearArbitraryTupleRelatedBy $ incrSize arbitrary
 
 instance ArbitraryOrderedTuple Rational where
-    arbitraryTupleRelatedBy = linearArbitraryTupleRelatedBy (incrSize arbitrary)
+    type (Area Rational) = (AreaLinear Int, AreaLinear Int)
+    areaWhole _ = (areaLinearWhole [-1,0,1], areaLinearWhole [0])
+    arbitraryTupleInAreaRelatedBy sample (numeratorArea, preDenominatorArea) = 
+        linearArbitraryTupleRelatedBy chooseRational
+        where
+        chooseRational = 
+            do
+            num <- arbitraryIntInArea numeratorArea
+            preDenom <- arbitraryIntInArea preDenominatorArea
+            return $ (toInteger num) % (1 + (abs $ toInteger preDenom))
+        arbitraryIntInArea = arbitraryLinear (minInt, maxInt) succ pred choose  
+        maxInt = maxBound
+        minInt = minBound
+    arbitraryTupleRelatedBy =
+        linearArbitraryTupleRelatedBy $ incrSize arbitrary
+
+--data AreaDouble =
+--    AreaDouble
+--    {
+--        areaDblExp :: AreaLinear Int,
+--        areaDblEncourageOne :: Bool,
+--        areaDblAllowPos :: Bool,
+--        areaDblAllowNeg :: Bool
+--    }
+    
+areaDoubleSmall :: AreaLinear Double
+areaDoubleSmall =
+    AreaLinear (Just $ -256) False (Just 256) False [0,-1,1]
 
 instance ArbitraryOrderedTuple Double where
-   arbitraryTupleRelatedBy = 
-       linearArbitraryTupleRelatedBy (incrSize $ chooseSmallDouble)
-       where
-       chooseSmallDouble =
-           do
-           d <- arbitrary
-           e1 <- choose (0,8)
-           e2 <- choose (-380,-1)
-           e <- elements [e1,e2]
-           s <- elements [1,1,1,1,0]
-           a <- elements [-1,0,1]
-           return (buildDouble d e s a)
-       buildDouble d e s a =
-           (s * dE + a)
-           where
-           dE = encodeFloat m (e - 53)
-           (m,_) = decodeFloat (d :: Double)
-           
+    type (Area Double) = AreaLinear Double
+    areaWhole _ = areaLinearWhole [-1/0,-1,0,1,1/0]
+    arbitraryTupleInAreaRelatedBy sample area = 
+        linearArbitraryTupleRelatedBy 
+                (arbitraryLinear (-maxDbl, maxDbl) id id chooseDbl area)
+        where
+        maxDbl = encodeFloat 1 (maxExp - 1)
+        chooseDbl (lb, ub) =
+            do
+            exp <- chooseNearerZero (expLb, expUb) -- TODO: prefer exp near 0
+            case exp == expUb of
+                True -> choose (lb, ub)
+                False -> chooseWithExp exp
+            where
+            chooseNearerZero (lo, hi) =
+--                unsafePrint ("chooseNearerZero: "
+--                    ++ "\n loDT = " ++ show loDT
+--                    ++ "\n hiDT = " ++ show hiDT
+--                ) $
+                do
+                eDT <- choose (loDT, hiDT)
+                return $ round $ transform eDT 
+                where
+                loDT = transformInv loD
+                hiDT = transformInv hiD
+                transform :: Double -> Double
+                transform x = x*x*x/1000000 -- 100^3 = 1000000
+                transformInv x 
+                    | x > 0 = 100 * exp ((log x)/3)
+                    | x < 0 = - (transformInv (-x))
+                    | otherwise = 0
+                loD = fromInteger $ toInteger lo
+                hiD = fromInteger $ toInteger hi
+            expLb = 
+                case (lb <= 0 && ub >= 0) of
+                    True -> minExp
+                    False -> min lbExp ubExp
+            expUb = max lbExp ubExp
+            lbExp = case lb == 0 of True -> minExp; False -> exponent lb
+            ubExp = case ub == 0 of True -> minExp; False -> exponent ub
+            chooseWithExp exp =
+                do
+                signif <- choose (0.5,2)
+                let validCandidates = deriveValidCandidates $ signif * (encodeFloat 1 exp)
+                case validCandidates of
+                    [] -> chooseWithExp exp
+                    _ -> elements validCandidates
+            deriveValidCandidates d =
+                filter valid [d,-d]
+                where
+                valid d = lb <= d && d <= ub
+        (minExp, maxExp) = floatRange sample
+    arbitraryTupleRelatedBy =
+        arbitraryTupleInAreaRelatedBy (0 :: Double) areaDoubleSmall
        -- When generating Double numbers for testing, try to avoid overflows
        -- as we cannot usually overcome overflows when we cannot increase 
        -- the granularity (aka precision) of the floating point type.
        -- Exp overflows at around 700.
+
+--        where
+--        AreaLinear mmin minStrict mmax maxStrict = bounds
+--        min = 
+--            case mmin of 
+--                Nothing -> 
+--                Just m -> 
+--                    case expMinStrict of
+--                        True -> m+1
+--                        False -> m
+--        expMax = 
+--            case mexpMax of 
+--                Nothing -> 480; -- encourage infinity 
+--                Just m -> 
+--                    case expMaxStrict of
+--                        True -> m-1
+--                        False -> m
+--        arbitaryBoundedDouble =
+--           do
+--           d <- arbitrary
+--           e <- case (expMin < 0 && expMax > 0) of
+--               True -> 
+--                   do 
+--                   e1 <- choose (0,expMax)
+--                   e2 <- choose (-380,-1)
+--                   elements [e1,e2]
+--               False -> choose (expMin, expMax)
+--           s <- elements $ case (encourage [1,1,1,1,0]
+--           a <- elements $ case (encourageZero, encourageOne) [-1,0,1]
+--           return (buildDouble d e s a)
+--        buildDouble d e s a =
+--           (s * dE + a)
+--           where
+--           dE = encodeFloat m (e - 53)
+--           (m,_) = decodeFloat (d :: Double)
+--           
 
 {-| Default implementation of linearArbitraryTupleRelatedBy for Ord instances -}   
 linearArbitraryTupleRelatedBy ::
@@ -125,7 +324,9 @@ linearArbitraryTupleRelatedBy givenArbitrary indices constraints =
                (_, Just pord) -> pord == EQ
 
 arbitraryPairRelatedBy ::
-    (ArbitraryOrderedTuple t) => PartialOrdering -> Maybe (Gen (t,t))
+    (ArbitraryOrderedTuple t) => 
+    PartialOrdering -> 
+    Maybe (Gen (t,t))
 arbitraryPairRelatedBy rel =
     case arbitraryTupleRelatedBy [1,2] [((1,2),[rel])] of
         Nothing -> Nothing
@@ -136,7 +337,8 @@ arbitraryPairRelatedBy rel =
 
 arbitraryTripleRelatedBy ::
     (ArbitraryOrderedTuple t) => 
-    (PartialOrdering, PartialOrdering, PartialOrdering) -> Maybe (Gen (t,t,t))
+    (PartialOrdering, PartialOrdering, PartialOrdering) -> 
+    Maybe (Gen (t,t,t))
 arbitraryTripleRelatedBy (r1, r2, r3) =
     case arbitraryTupleRelatedBy [1,2,3] constraints of
         Nothing -> Nothing
