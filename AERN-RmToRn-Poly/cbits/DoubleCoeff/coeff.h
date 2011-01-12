01c51aa602ceb9357c217e877976107e51fa267f
mikkonecny <devnull@localhost>
2011-01-12 22:52:50 +0000

    Commented out all references to mutable double coefficients in C, this distinction will  be made only at the Haskell level.

--- AERN-RmToRn-Poly/cbits/DoubleCoeff/coeff.h
+++ AERN-RmToRn-Poly/cbits/DoubleCoeff/coeff.h
@@ -11,21 +11,21 @@
 #include <fenv.h>
 
 /* The following are provided for better code readability: */
+typedef double ConversionOp(double);
+
+#define CF_CONVERT(convOp,d) (convOp(d))
+
 typedef int ComparisonOp(double, double);
 
 #define CF_COMPARE(op, d1, d2) (((d1)>(d2))? 1 : ((d1)<(d2)? -1 : 0)
 
 typedef double Coeff;
-typedef double * UnaryOp(double);
-typedef double * BinaryOp(double, double);
 
 typedef double CoeffMutable;
    // deliberately not introducing a pointer here - in C a normal
    // variable is also mutable; we will ensure
 typedef double NewOpMutable(double);
 typedef double CloneOpMutable(double);
-typedef void * UnaryOpMutable(double *, double *);
-typedef void * BinaryOpMutable(double *, double *, double *);
 
 /* a dummy type shadowing an important type in the generic coefficient version */
 typedef struct OPS_PURE
@@ -49,27 +49,31 @@ typedef struct OPS_PURE
 #define CF_FREE(d) (1) // signal success without doing anything
 #define CF_CLONE(d) (d) // not a pointed type, normal assignment clones
 
-/* a dummy type shadowing an important type in the generic coefficient version */
-typedef struct OPS_MUTABLE
-{
-} Ops_Mutable;
+// there is no need to distinguish a polynomial with mutable coefficients
+// from one with immutable coefficients in the case of double implemented
+// purely in C; this distinction makes sense only in Haskell
 
-/* the following macros give direct operation specification
- * instead of using an instance of the above type, which is
- * what has to be done for the generic coefficient version */
-#define CFM_SAMPLE(ops) (1.0)
-#define CFM_NEW(ops) (1.0)
-#define CFM_CLONE(ops,rp,sp) ((rp)=(sp))
-#define CFM_ASSIGN(ops,rp,sp) ((rp)=(sp))
-#define CFM_ABS_UP(ops,rp,dp) ((rp)=CF_ABS_UP(ops,dp))
-#define CFM_ABS_DN(ops,rp,dp) ((rp)=CF_ABS_DN(ops,dp))
-#define CFM_ADD_UP(ops,rp,dp1,dp2) ((rp)=CF_ADD_UP(ops,dp1,dp2))
-#define CFM_ADD_DN(ops,rp,dp1,dp2) ((rp)=CF_ADD_DN(ops,dp1,dp2))
-#define CFM_SUB_UP(ops,rp,dp1,dp2) ((rp)=CF_SUB_UP(ops,dp1,dp2))
-#define CFM_SUB_DN(ops,rp,dp1,dp2) ((rp)=CF_SUB_DN(ops,dp1,dp2))
-#define CFM_MUL_UP(ops,rp,dp1,dp2) ((rp)=CF_MUL_UP(ops,dp1,dp2))
-#define CFM_MUL_DN(ops,rp,dp1,dp2) ((rp)=CF_MUL_DN(ops,dp1,dp2))
-
-#define CFM_FREE(dp) (1)
+///* a dummy type shadowing an important type in the generic coefficient version */
+//typedef struct OPS_MUTABLE
+//{
+//} Ops_Mutable;
+//
+///* the following macros give direct operation specification
+// * instead of using an instance of the above type, which is
+// * what has to be done for the generic coefficient version */
+//#define CFM_SAMPLE(ops) (1.0)
+//#define CFM_NEW(ops) (1.0)
+//#define CFM_CLONE(ops,rp,sp) ((rp)=(sp))
+//#define CFM_ASSIGN(ops,rp,sp) ((rp)=(sp))
+//#define CFM_ABS_UP(ops,rp,dp) ((rp)=CF_ABS_UP(ops,dp))
+//#define CFM_ABS_DN(ops,rp,dp) ((rp)=CF_ABS_DN(ops,dp))
+//#define CFM_ADD_UP(ops,rp,dp1,dp2) ((rp)=CF_ADD_UP(ops,dp1,dp2))
+//#define CFM_ADD_DN(ops,rp,dp1,dp2) ((rp)=CF_ADD_DN(ops,dp1,dp2))
+//#define CFM_SUB_UP(ops,rp,dp1,dp2) ((rp)=CF_SUB_UP(ops,dp1,dp2))
+//#define CFM_SUB_DN(ops,rp,dp1,dp2) ((rp)=CF_SUB_DN(ops,dp1,dp2))
+//#define CFM_MUL_UP(ops,rp,dp1,dp2) ((rp)=CF_MUL_UP(ops,dp1,dp2))
+//#define CFM_MUL_DN(ops,rp,dp1,dp2) ((rp)=CF_MUL_DN(ops,dp1,dp2))
+//
+//#define CFM_FREE(dp) (1)
 
 #endif /* COEFF_H_ */
