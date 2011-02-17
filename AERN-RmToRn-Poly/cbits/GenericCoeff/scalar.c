7cb8d9c7cc4086b5d32440c029f3a1c5f6341d73
Jan Duracz <jan@duracz.net>
2011-02-17 16:58:04 +0000

    Added mutable versions of scaling functions to cbits/GenCoeff, tests pending.

--- AERN-RmToRn-Poly/cbits/GenericCoeff/scalar.c
+++ AERN-RmToRn-Poly/cbits/GenericCoeff/scalar.c
@@ -9,6 +9,80 @@
 #include "GenericCoeff/poly.h"
 
 void
+ADD_COEFF_CODE(scaleTermsUsingMutableOps)(Ops_Mutable * ops, Coeff c, Poly * p)
+{
+  Var pSize = p -> psize;
+  if (pSize > 0) // any terms to scale?
+  {
+    Term * terms = p -> terms;
+
+    Coeff oldCoeff;
+    Coeff coeffScaledUp;
+    Coeff coeffScaledDn;
+    Coeff maxError;
+    Coeff errorBound;
+    for (int i = 0; i < pSize; i++)
+    {
+      CFM_ASSIGN_VAL(ops, oldCoeff, terms[i].coeff); // fetch coefficient value
+      CFM_MUL_UP(ops, coeffScaledUp, c, oldCoeff); // scale coefficient
+      CFM_MUL_DN(ops, coeffScaledDn, c, oldCoeff);
+      CFM_ASSIGN_VAL(ops, terms[i].coeff, coeffScaledUp); // update coefficient
+      // note that we choose the upward rounded scaled constant term, can cause upward drift
+      CFM_SUB_UP(ops, maxError, coeffScaledUp, coeffScaledDn); // bound rounding error
+      CFM_ADD_UP(ops, errorBound, errorBound, maxError); // accumulate rounding error
+    }
+    CFM_FREE(oldCoeff);
+    CFM_FREE(coeffScaledUp);
+    CFM_FREE(coeffScaledDn);
+    CFM_FREE(maxError);
+    CFM_ASSIGN_VAL(ops, p -> errorBound, errorBound); // account for rounding errors
+    CFM_FREE(errorBound);
+  }
+}
+
+void
+ADD_COEFF_CODE(scaleUpThinUsingMutableOps)(Coeff zero, Ops_Mutable * ops, Coeff c, Poly * p)
+{
+  ADD_COEFF_CODE(scaleTermsUsingMutableOps)(ops, c, p); // scale non-constant terms
+  Coeff constTerm;
+  CFM_MUL_UP(ops, constTerm, c, p -> constTerm); // scale constTerm up
+  CFM_ADD_UP(ops, p -> constTerm, constTerm, p -> errorBound); // account for errorBound
+  CFM_FREE(constTerm);
+  CFM_ASSIGN_VAL(ops, p -> errorBound, zero); // collapse errorBound
+}
+
+void
+ADD_COEFF_CODE(scaleDnThinUsingMutableOps)(Coeff zero, Ops_Mutable * ops, Coeff c, Poly * p)
+{
+  ADD_COEFF_CODE(scaleTermsUsingMutableOps)(ops, c, p); // scale non-constant terms
+  Coeff constTerm;
+  CFM_MUL_DN(ops, constTerm, c, p -> constTerm); // scale constTerm down
+  CFM_SUB_DN(ops, p -> constTerm, constTerm, p -> errorBound); // account for errorBound
+  CFM_FREE(constTerm);
+  CFM_ASSIGN_VAL(ops, p -> errorBound, zero); // collapse errorBound
+}
+
+void
+ADD_COEFF_CODE(scaleEnclUsingMutableOps)(Ops_Mutable * ops, Coeff c, Poly * p)
+{
+  ADD_COEFF_CODE(scaleTermsUsingMutableOps)(ops, c, p); // scale non-constant terms
+  Coeff oldConstTerm;
+  Coeff constTermScaledUp;
+  Coeff constTermScaledDn;
+  Coeff maxError;
+  CFM_ASSIGN_VAL(ops, oldConstTerm, p -> constTerm); // fetch constTerm value
+  CFM_MUL_UP(ops, constTermScaledUp, c, oldConstTerm); // scale constTerm
+  CFM_MUL_DN(ops, constTermScaledDn, c, oldConstTerm);
+  CFM_FREE(oldConstTerm);
+  CFM_ASSIGN_VAL(ops, p -> constTerm, constTermScaledUp); // update constTerm
+  CFM_SUB_UP(ops, maxError, constTermScaledUp, constTermScaledDn); // bound rounding error
+  CFM_FREE(constTermScaledUp);
+  CFM_FREE(constTermScaledDn);
+  CFM_ADD_UP(ops, p -> errorBound, p -> errorBound, maxError); // account for rounding error
+  CFM_FREE(maxError);
+}
+
+void
 ADD_COEFF_CODE(scaleTermsUsingPureOps)(Ops_Pure * ops, Coeff c, Poly * p)
 {
   Term * terms = p -> terms;
@@ -21,7 +95,7 @@ ADD_COEFF_CODE(scaleTermsUsingPureOps)(Ops_Pure * ops, Coeff c, Poly * p)
 	Coeff coeffScaledDn = CF_MUL_DN(ops, c, oldCoeff);
 	CF_FREE(oldCoeff);
 	terms[i].coeff = coeffScaledUp; // scale term coefficient
-	Coeff maxError = CF_SUB_UP(ops, coeffScaledUp, coeffScaledDn); // >= 0
+	Coeff maxError = CF_SUB_UP(ops, coeffScaledUp, coeffScaledDn); // bound rounding error
 	CF_FREE(coeffScaledUp);
 	CF_FREE(coeffScaledDn);
 	Coeff oldErrorBound = p -> errorBound;
@@ -69,12 +143,12 @@ ADD_COEFF_CODE(scaleEnclUsingPureOps)(Ops_Pure * ops, Coeff c, Poly * p)
   Coeff constTermScaledDn = CF_MUL_DN(ops, c, oldConstTerm);
   CF_FREE(oldConstTerm);
   p -> constTerm = constTermScaledUp; // scale the constant term
-  // note that we choose the upwards rounded scaled constant term, could cause upward drift
+  // note that we choose the upward rounded scaled constant term, can cause upward drift
   Coeff maxError = CF_SUB_UP(ops, constTermScaledUp, constTermScaledDn);
   CF_FREE(constTermScaledUp);
   CF_FREE(constTermScaledDn);
   Coeff oldErrorBound = p -> errorBound;
-  p -> errorBound = CF_ADD_UP(ops, oldErrorBound, maxError); // increase error
+  p -> errorBound = CF_ADD_UP(ops, oldErrorBound, maxError); // account for rounding error
   CF_FREE(oldErrorBound);
   CF_FREE(maxError);
 }
