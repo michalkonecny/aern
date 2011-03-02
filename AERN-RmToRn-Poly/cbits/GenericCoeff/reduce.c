892045d91e61027e755b4df0f9ac72d1a76f9d32
janduracz <devnull@localhost>
2011-03-02 01:08:14 +0000

    Added last case in copyEncl and commented it out but now test segfaults.

--- AERN-RmToRn-Poly/cbits/GenericCoeff/reduce.c
+++ AERN-RmToRn-Poly/cbits/GenericCoeff/reduce.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <tree234.h>
 
 #include <GenericCoeff/coeff.h>
 #include <GenericCoeff/poly.h>
@@ -24,13 +25,21 @@ ADD_COEFF_CODE(copyTermsWithoutReduction)(Ops_Mutable * ops, Var arity,
   res -> psize = size; // forget remaining terms in res
 }
 
+//typedef struct { CoeffMutable coeff; int index; ComparisonOp compare; } CoeffFor234;
+//
+//int compareFor234(CoeffFor234 * dp1, CoeffFor234 * dp2)
+//{
+//  return CF_COMPARE(dp1 -> compare, dp1 -> coeff, dp2 -> coeff);
+//}
+
 /*
  * ASSUMES: maxArity and maxSize of src and res are the same
- * INVARIANT : leaves maxDeg of res unchanged
+ * INVARIANT : does not change maxDeg and does not increase psize of res
  */
 
 void
-ADD_COEFF_CODE(copyEnclUsingMutableOps)(Ops_Mutable * ops, Poly * res, Poly * src)
+ADD_COEFF_CODE(copyEnclUsingMutableOps)(ComparisonOp compare, Ops_Mutable * ops,
+    Poly * res, Poly * src)
 {
   Var arity = src -> maxArity;
   Size size = src -> psize;
@@ -43,6 +52,7 @@ ADD_COEFF_CODE(copyEnclUsingMutableOps)(Ops_Mutable * ops, Poly * res, Poly * sr
 
   CFM_ASSIGN(ops, res -> constTerm, src -> constTerm);
 
+  printf("0/n");
   if (deg <= resMaxDeg && size <= resSize) // just copy src terms into res?
   {
     ADD_COEFF_CODE(copyTermsWithoutReduction)(ops, arity,
@@ -68,45 +78,88 @@ ADD_COEFF_CODE(copyEnclUsingMutableOps)(Ops_Mutable * ops, Poly * res, Poly * sr
           src, terms, size);
       CFM_ASSIGN(ops, res -> errorBound, errorBound);
     }
-//    else // some reduction will be needed
+//    else // size-resSize reductions are needed
 //    {
+//      tree234 * tree = newtree234(&compareFor234);  // get a tree
+//      int last = size-resSize-1; // index of last element in the full tree
+//      while (size > resSize) // fill the tree
+//      {
+//        size--; // now size == index of last term in degree reduced src
+//        CoeffFor234 * c = malloc(sizeof(CoeffFor234));
+//        CFM_ABS_UP(ops, c -> coeff, terms[size].coeff);
+//        c -> index = size;
+//        c -> compare = compare;
+//        add234(tree, c);
+//      }
+//      int nextResTermIndex = 0;
+//      while (size > 0) // add one and delete last from tree, using it to copy to res
+//      {
+//        size--; //
+//        CoeffFor234 * c = malloc(sizeof(CoeffFor234));
+//        CFM_ABS_UP(ops, c -> coeff, terms[size].coeff);
+//        c -> index = size;
+//        c -> compare = compare;
+//        add234(tree, c);
+//        CoeffFor234 * oldAbsCoeff = (CoeffFor234 *)delpos234(tree, last);
 //
+//        // copy terms[oldAbsCoeff -> index] into resTerms[nextResTermIndex]
+//        CFM_ASSIGN(ops, resTerms[nextResTermIndex].coeff, terms[oldAbsCoeff -> index].coeff);
+//        memmove(resTerms[nextResTermIndex].powers, terms[oldAbsCoeff -> index].powers, SIZEOF_POWERS(arity));
+//
+//        CFM_FREE(oldAbsCoeff -> coeff); // free the deleted tree element's coefficient
+//        free(oldAbsCoeff); // and the deleted element
+//      }
+//      while (last >= 0) // accumulate the reduction errors while emptying the tree
+//      {
+//        CoeffFor234 * oldAbsCoeff = (CoeffFor234 *)delpos234(tree, last);
+//        CFM_ADD_UP(ops, errorBound, errorBound, oldAbsCoeff -> coeff);
+//        CFM_FREE(oldAbsCoeff -> coeff);
+//        last--;
+//      }
+//      CFM_ASSIGN(ops, res -> errorBound, errorBound);
 //    }
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//tree234 *
+//ADD_COEFF_CODE(markTermsWithDegreeBelowAndLargestCoeffs)(ComparisonOp compare, Ops_Pure * ops,
+//    Term ** termsArray, Size termCount, Size maxSize, Power maxDegree)
+//{
+//  tree234 * markUs = newtree234(&compareFor234);
+//  Term * terms = *termsArray;
+//  Power degree = 0;
+//  int i = 0;
+//  // !!! add int counting the actual number of terms added to markUs
+//  // !!! make sure loops iterate the right number of times.
+//  while (i < maxSize)
+//  {
+//    if (terms[i].degree <= maxDegree) // anything to do?
+//    {
+//      CoeffFor234 * c = malloc(sizeof(CoeffFor234));
+//      c -> coeff = CF_ABS_UP(ops, terms[i].coeff);
+//      c -> index = i;
+//      c -> compare = compare;
+//      add234(markUs, c);
+//    }
+//    i++;
+//  }
+//  while (i < termCount)
+//  {
+//    if (terms[i].degree <= maxDegree) // anything to do?
+//    {
+//      CoeffFor234 * c = malloc(sizeof(CoeffFor234));
+//      c -> coeff = CF_ABS_UP(ops, terms[i].coeff);
+//      c -> index = i;
+//      c -> compare = compare;
+//      add234(markUs, c);
+//      CoeffFor234 * oldAbsCoeff = (CoeffFor234 *)delpos234(markUs, 0);
+//      CF_FREE(oldAbsCoeff -> coeff);
+//      free(oldAbsCoeff);
+//    }
+//    i++;
+//  }
+//  return markUs;
+//}
 
 void
 ADD_COEFF_CODE(reduceDegreeEnclUsingMutableOps)(Ops_Mutable * ops, Power maxDeg, Poly * p)
@@ -149,118 +202,3 @@ ADD_COEFF_CODE(reduceDegreeEnclUsingMutableOps)(Ops_Mutable * ops, Power maxDeg,
     p -> psize = lastTermIndex; // update psize
   }
 }
-
-/*
-
-void
-ADD_COEFF_CODE(reduceDegreeUsingPureOps)(Ops_Pure * ops, Power maxDeg, Poly * p)
-{
-  Term * terms = p -> terms;
-  Size oldSize = p -> psize;
-  Size lastTermIndex = p -> psize - 1; // track last term for swapping
-  for (int i = 0; i <= lastTermIndex; i++)
-  {
-    if (terms[i].degree > maxDeg) // reduce term?
-    {
-      // QUESTION: cheaper to use a local Coeff coeffi = terms[i].coeff ?
-
-      Coeff maxError = CF_ABS_UP(ops, terms[i].coeff); // compute reduction error
-      CF_FREE(terms[i].coeff); // free ith coefficient
-
-      // account for reduction error
-      Coeff oldErrorBound = p -> errorBound;
-      p -> errorBound = CF_ADD_UP(ops, oldErrorBound, maxError);
-      CF_FREE(oldErrorBound);
-      CF_FREE(maxError);
-
-      // shift terms cheaply by swapping the ith and the last term
-      terms[i].degree = terms[lastTermIndex].degree; // overwrite ith degree with last
-      terms[i].powers = terms[lastTermIndex].powers; // point ith power to last
-      terms[i].coeff = terms[lastTermIndex].coeff; // point ith coefficient to last
-      lastTermIndex--; // forget last term, i.e. decrement psize (implicitly)
-    }
-  }
-  if (terms[lastTermIndex].degree > maxDeg) // reduce last term?
-  {
-    // QUESTION: cheaper to use a local Coeff coeffLast = terms[lastTermIndex].coeff ?
-
-    Coeff maxError = CF_ABS_UP(ops, terms[lastTermIndex].coeff); // compute reduction error
-    CF_FREE(terms[lastTermIndex].coeff); // free last coefficient
-
-    // account for reduction error
-    Coeff oldErrorBound = p -> errorBound;
-    p -> errorBound = CF_ADD_UP(ops, oldErrorBound, maxError);
-    CF_FREE(oldErrorBound);
-    CF_FREE(maxError);
-
-    lastTermIndex--; // forget last term, i.e. decrement psize (implicitly)
-  }
-  lastTermIndex++; // set lastTermIndex to psize
-  if (oldSize > lastTermIndex) // any terms reduced?
-  {
-    p -> psize = lastTermIndex; // update psize
-  }
-}
-
-typedef struct { Coeff coeff; int index; ComparisonOp compare; } CoeffFor234;
-
-int compareFor234(CoeffFor234 * dp1, CoeffFor234 * dp2)
-{
-  return CF_COMPARE(dp1 -> compare, dp1 -> coeff, dp2 -> coeff);
-}
-
-tree234 *
-ADD_COEFF_CODE(markTermsWithDegreeBelowAndLargestCoeffs)(ComparisonOp compare, Ops_Pure * ops,
-    Term ** termsArray, Size termCount, Size maxSize, Power maxDegree)
-{
-  tree234 * markUs = newtree234(&compareFor234);
-  Term * terms = *termsArray;
-  Power degree = 0;
-  int i = 0;
-  // !!! add int counting the actual number of terms added to markUs
-  // !!! make sure loops iterate the right number of times.
-  while (i < maxSize)
-  {
-    if (terms[i].degree <= maxDegree) // anything to do?
-    {
-      CoeffFor234 * c = malloc(sizeof(CoeffFor234));
-      c -> coeff = CF_ABS_UP(ops, terms[i].coeff);
-      c -> index = i;
-      c -> compare = compare;
-      add234(markUs, c);
-    }
-    i++;
-  }
-  while (i < termCount)
-  {
-    if (terms[i].degree <= maxDegree) // anything to do?
-    {
-      CoeffFor234 * c = malloc(sizeof(CoeffFor234));
-      c -> coeff = CF_ABS_UP(ops, terms[i].coeff);
-      c -> index = i;
-      c -> compare = compare;
-      add234(markUs, c);
-      CoeffFor234 * oldAbsCoeff = (CoeffFor234 *)delpos234(markUs, 0);
-      CF_FREE(oldAbsCoeff -> coeff);
-      free(oldAbsCoeff);
-    }
-    i++;
-  }
-  return markUs;
-}
-
-void ADD_COEFF_CODE(copy)(ComparisonOp compare, Ops_Pure * ops,
-    Poly * res, Poly * src)
-{
-  Size srcSize = src -> psize;
-  Term * srcTerms = src -> terms;
-
-  Size resSize = res -> psize;
-  Power maxDeg = res -> maxDeg;
-
-  tree234 * termsToCopy =
-    markTermsWithDegreeBelowAndLargestCoeffsGenCf(compare, ops,
-      srcTerms, srcSize, resSize, maxDeg);
-}
-
-*/
