4d2ac9fbb0cb97b16b36cb1a85ee8fbe47575f22
Jan Duracz <jan@duracz.net>
2011-03-03 00:11:45 +0000

    another debugging commit.

--- AERN-RmToRn-Poly/tests/Main.hs
+++ AERN-RmToRn-Poly/tests/Main.hs
@@ -208,17 +208,17 @@ testMutableGCPolys =
     putStrLn $ "scaleDnThin 0.1 x = " ++ showP sdx
     putStrLn $ "scaleEncl 0.1 x = " ++ showP sex
     putStrLn $ "reduceDegreeEncl 0 pb223 = " ++ showP rd0pb223
-    putStrLn $ "copyEncl 0 pb223 = " ++ showP cp0pb223
+    putStrLn $ "copyEncl 0 y = " ++ showP cpres
     where
     showP = showInternals (showChebTerms, showCoeffInternals)
     showChebTerms = True
     showCoeffInternals = False
     opsPtr = GCPoly.newOpsPureArithUpDnDefaultEffort sampleD
     opsMutablePtr = GCPoly.newOpsMutableArithUpDnDefaultEffort sampleD
-    [p1,p2,p3,p11,p12,p22,p1b23,pb223,p23s1,pb223s1,pb223d0,sux,sdx,sex,rd0pb223,cp0pb223] = runST $
+    [p1,p2,p3,p11,p12,p22,p1b23,pb223,p23s1,pb223s1,pb223d0,sux,sdx,sex,rd0pb223,cpres] = runST $
         do
         let mkConst c = GCPoly.constPolyMutable (c::Double) 0 (Var 2) (Size 10) (Power 3)
-        let mkConstConst c = GCPoly.constPolyMutable (c::Double) 0 (Var 2) (Size 1) (Power 3)
+        let mkConstConst c = GCPoly.constPolyMutable (c::Double) 0 (Var 2) (Size 10) (Power 3)
         let mkVar n = GCPoly.projectionPolyMutable sampleD (Var n) (Var 2) (Size 10) (Power 3)
         let addUp = GCPoly.polyAddUpMutableUsingMutableOps sampleD opsMutablePtr
         let scaleUpThin c = GCPoly.polyScaleUpMutableUsingMutableOps 0 opsMutablePtr (c::Double) 
@@ -257,9 +257,9 @@ testMutableGCPolys =
         scaleEncl 2.0 rd0pb223M
         addUp rd0pb223M p3M rd0pb223M
         reduceDegree 0 rd0pb223M
-        cp0pb223M <- mkConstConst 0
-        copyEncl cp0pb223M pb223M
+        cpresM <- mkVar 0 
+        copyEncl cpresM p3M  -- copyEncl (Var 0) pb223M also shows bug
         mapM (GCPoly.unsafeReadPolyMutable sampleD) 
           [p1M, p2M, p3M, p11M, p12M, p22M, p1b23M, pb223M, p23s1M, pb223s1M, pb223d0M, 
-           suxM, sdxM, sexM, rd0pb223M, cp0pb223M]
+           suxM, sdxM, sexM, rd0pb223M, cpresM]
  
