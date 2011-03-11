e2eef53b7f16f15fadb429700690fbdc2b933647
Michal Konečný <mikkonecny@gmail.com>
2011-03-11 16:56:46 +0000

    first version of MinimalFnBasis class that each version of the C-core should implement, the rest of the function arithmetic should depend on this class only

---
+++ AERN-RmToRn/src/Numeric/AERN/RmToRn/MinimalFnBasis.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-|
+    Module      :  Numeric.AERN.RmToRn.MinimalFnBasis
+    Description :  class specifying a core function basis on a fixed domain  
+    Copyright   :  (c) Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+    
+    Class specifying a core function basis on a fixed domain.
+    
+    This module should be only used by libraries that provide
+    an alternative function basis.
+-}
+
+module Numeric.AERN.RmToRn.MinimalFnBasis where
+
+import Numeric.AERN.RmToRn.Domain
+import Numeric.AERN.RmToRn.New
+import Numeric.AERN.RmToRn.Evaluate
+
+import Numeric.AERN.Basics.Interval
+
+import qualified Numeric.AERN.RealArithmetic.NumericOrderRounding as ArithUpDn
+--import qualified Numeric.AERN.Basics.NumericOrder as NumOrd
+
+class (HasDomainBox fb,
+--        CanEvaluate fb, -- value at a point
+--        CanSubstitute fb, -- substitution
+        CanEvaluateOtherType fb, -- another interpretation (eg string or interval)
+        HasProjections fb, -- variables (but their domain is fixed!)
+        ArithUpDn.Convertible (Domain fb) fb, -- constants
+        ArithUpDn.Convertible fb (Domain fb), -- bounds
+        ArithUpDn.RoundedAdd fb,
+        ArithUpDn.RoundedMultiply fb,
+        ArithUpDn.RoundedMixedAdd fb (Domain fb),
+        ArithUpDn.RoundedMixedMultiply fb (Domain fb)
+      ) => 
+    MinimalFnBasis fb
+    where
+    fixedDomain :: fb -> Interval (Domain fb)
+    
