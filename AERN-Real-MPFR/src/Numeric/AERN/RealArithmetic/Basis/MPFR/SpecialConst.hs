11fcbf83dbd575664154308a9b76c350abfc6460
Michal Konečný <mikkonecny@gmail.com>
2011-03-24 11:35:00 +0000

    Added type class of special constants, currently including only pi and e + Double, MPFR and Interval implementations

---
+++ AERN-Real-MPFR/src/Numeric/AERN/RealArithmetic/Basis/MPFR/SpecialConst.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE TypeFamilies #-}
+{-|
+    Module      :  Numeric.AERN.RealArithmetic.Basis.MPFR.SpecialConst
+    Description :  common constants such as pi
+    Copyright   :  (c) Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+
+    Common constants such as pi.
+-}
+
+module Numeric.AERN.RealArithmetic.Basis.MPFR.SpecialConst where
+
+import Numeric.AERN.RealArithmetic.NumericOrderRounding
+
+import qualified Data.Number.MPFR as M
+import Data.Number.MPFR (MPFR)
+
+instance  
+    RoundedSpecialConstEffort MPFR
+    where
+    type SpecialConstEffortIndicator MPFR = M.Precision
+    specialConstDefaultEffort _ = 100
+
+instance 
+    RoundedSpecialConst MPFR
+    where
+    piUpEff prec = M.pi M.Up prec
+    piDnEff prec = M.pi M.Down prec
+    eUpEff prec = M.exp M.Up prec 1
+    eDnEff prec = M.exp M.Down prec 1
---
+++ AERN-Real/src/Numeric/AERN/RealArithmetic/NumericOrderRounding/SpecialConst.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ImplicitParams #-}
+{-|
+    Module      :  Numeric.AERN.RealArithmetic.NumericOrderRounding.SpecialConst
+    Description :  support for common constants such as pi
+    Copyright   :  (c) Michal Konecny
+    License     :  BSD3
+
+    Maintainer  :  mikkonecny@gmail.com
+    Stability   :  experimental
+    Portability :  portable
+    
+    Support for common constants such as pi.
+    
+    This module is hidden and reexported via its parent NumericOrderRounding. 
+-}
+
+module Numeric.AERN.RealArithmetic.NumericOrderRounding.SpecialConst where
+
+--import Numeric.AERN.Basics.Effort
+--import Numeric.AERN.Basics.Exception
+--import Numeric.AERN.Basics.ShowInternals
+--import Numeric.AERN.RealArithmetic.Laws
+--import Numeric.AERN.RealArithmetic.Measures
+--import qualified Numeric.AERN.Basics.NumericOrder as NumOrd
+--import Numeric.AERN.Basics.NumericOrder.OpsImplicitEffort
+--
+--import Numeric.AERN.Misc.Debug
+--
+--import Test.QuickCheck
+--import Test.Framework (testGroup, Test)
+--import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+class RoundedSpecialConstEffort t where
+    type SpecialConstEffortIndicator t
+    specialConstDefaultEffort :: t -> SpecialConstEffortIndicator t
+
+class (RoundedSpecialConstEffort t) => RoundedSpecialConst t where
+    piUpEff :: (SpecialConstEffortIndicator t) -> t
+    piDnEff :: (SpecialConstEffortIndicator t) -> t
+    eUpEff :: (SpecialConstEffortIndicator t) -> t
+    eDnEff :: (SpecialConstEffortIndicator t) -> t
+
