94d024873b87486a5b7549ba2dc792cf7a7876fe
jan.duracz <devnull@localhost>
2011-04-23 16:05:48 +0100

    Mid-debug commit of quadraticInPlace demo

--- AERN-Real-Double/demos/Quadratic.hs
+++ AERN-Real-Double/demos/Quadratic.hs
@@ -2,8 +2,9 @@
 module Main where
 
 import Numeric.AERN.DoubleBasis.Interval
-import Data.List
+import Numeric.AERN.DoubleBasis.MInterval
 
+import Control.Monad.ST (runST)
 
 main = do
   putStrLn $ "quadratic 1 1 1 = " ++ show (quadratic 1 1 1)
@@ -17,16 +18,107 @@ main = do
 quadratic :: DI -> DI -> DI -> [DI]
 quadratic a b c 
   | certainlyZero discriminant =
-    [-b/(2*a)]
+    [doubleRoot]
   | certainlyNonnegative discriminant =
-    [(-b-sqrt discriminant)/(2*a),
-     (-b+sqrt discriminant)/(2*a)]
+    [leftRoot,rightRoot]
   | certainlyNegative discriminant =
     []
   | otherwise =
     [bottom]
   where
   discriminant = b^2-4*a*c
+  doubleRoot = -b/(2*a)
+  leftRoot = doubleRoot-sqrtDiscriminant/(2*a)  -- (-b-sqrt(b^2-4*a*c))/(2*a)
+  rightRoot = doubleRoot+sqrtDiscriminant/(2*a) -- (-b+sqrt(b^2-4*a*c))/(2*a)
+  sqrtDiscriminant = sqrt discriminant
+
+-- |
+-- In-place quadratic. 
+quadraticInPlace :: DI -> DI -> DI -> [DI]
+quadraticInPlace a b c
+  | certainlyZero discriminant =
+    [doubleRoot]
+  | certainlyNonnegative discriminant =
+    [leftRoot,rightRoot]
+  | certainlyNegative discriminant =
+    []
+  | otherwise =
+    [bottom]
+  where
+  discriminant = 
+    runST $
+      do
+      [aM,bM,cM] <- mapM makeMutable [a,b,c]
+      bM <^>= 2  -- b^2
+      aM <*>|= 4 -- 4*a
+      aM <*>= cM -- 4*a*c
+      bM <->= aM -- b^2-4*a*c
+      result <- readMutable bM
+      return result
+  doubleRoot =
+    runST $
+      do
+      [aM,bM] <- mapM makeMutable [a,b] 
+      bM </>|= (-2) -- -b/2 
+      bM </>= aM    -- -b/(2*a)
+      result <- readMutable bM
+      return result
+  [leftRoot,rightRoot] =
+    runST $
+      do
+      [aM,drM,diM] <- mapM makeMutable [a,doubleRoot,discriminant]
+      sqrtOutInPlace diM diM -- sqrt(b^2-4*a*c)
+      diM </>|= 2         -- (sqrt(b^2-4*a*c))/2 
+      diM </>= aM         -- (sqrt(b^2-4*a*c))/(2*a)
+      assignMutable aM drM
+      aM <->= diM         -- (-b-sqrt(b^2-4*a*c))/(2*a)
+      drM <+>= diM        -- (-b+sqrt(b^2-4*a*c))/(2*a)
+      result <- mapM_ readMutable [aM,drM]
+      return result
+
+-- |
+-- Space-optimised version of in-place quadratic. 
+quadraticInPlaceSmall :: DI -> DI -> DI -> [DI]
+quadraticInPlaceSmall a b c
+  | certainlyZero discriminant =
+    [doubleRoot]
+  | certainlyNonnegative discriminant =
+    [leftRoot,rightRoot]
+  | certainlyNegative discriminant =
+    []
+  | otherwise =
+    [bottom]
+  where
+  discriminant = 
+    runST $
+      do
+      [aM,bM,cM] <- mapM makeMutable [a,b,c]
+      bM <^>= 2  -- b^2
+      aM <*>|= 4 -- 4*a
+      aM <*>= cM -- 4*a*c
+      bM <->= aM -- b^2-4*a*c
+      result <- readMutable bM
+      return result
+  doubleRoot =
+    runST $
+      do
+      [aM,bM] <- mapM makeMutable [a,b] 
+      bM </>|= (-2) -- -b/2 
+      bM </>= aM    -- -b/(2*a)
+      result <- readMutable bM
+      return result
+  [leftRoot,rightRoot] =
+    runST $
+      do
+      [aM,drM,diM] <- mapM makeMutable [a,doubleRoot,discriminant]
+      sqrtOutInPlace diM diM -- sqrt(b^2-4*a*c)
+      diM </>|= 2         -- (sqrt(b^2-4*a*c))/2 
+      diM </>= aM         -- (sqrt(b^2-4*a*c))/(2*a)
+      assignMutable aM drM
+      aM <->= diM         -- (-b-sqrt(b^2-4*a*c))/(2*a)
+      drM <+>= diM        -- (-b+sqrt(b^2-4*a*c))/(2*a)
+      result <- mapM_ readMutable [aM,drM]
+      return result
 
 certainlyZero x =
   case x ==? 0 of
