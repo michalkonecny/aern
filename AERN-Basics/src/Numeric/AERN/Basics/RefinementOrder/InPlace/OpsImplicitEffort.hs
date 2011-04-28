063dfa5687aecf298df2d14836621235ce4c61c0
jan.duracz <devnull@localhost>
2011-04-28 10:53:58 +0100

    Corrected names of operations and added assignment versions in ...Basics.RefOrd.InPlace.OpsImplicitEffort.

--- AERN-Basics/src/Numeric/AERN/Basics/RefinementOrder/InPlace/OpsImplicitEffort.hs
+++ AERN-Basics/src/Numeric/AERN/Basics/RefinementOrder/InPlace/OpsImplicitEffort.hs
@@ -15,58 +15,89 @@
 module Numeric.AERN.Basics.RefinementOrder.InPlace.OpsImplicitEffort where
 
 import Numeric.AERN.Basics.Mutable
-import qualified Numeric.AERN.Basics.NumericOrder as NumOrd
 import Numeric.AERN.Basics.RefinementOrder
 
-infixr 3 </\>, <⊓>, >/\<, >⊓< 
-infixr 2 <\/>, <⊔>, >\/<, >⊔< 
+infixr 3 </\>=, <⊓>=, >/\<=, >⊓<= 
+infixr 2 <\/>=, <⊔>=, >\/<=, >⊔<= 
 
 -- | Outward rounded in-place join
-(<\/>) :: 
+joinOutInPlace :: 
     (OuterRoundedLatticeInPlace t, 
      ?joinmeetOutEffort :: JoinMeetOutEffortIndicator t) => 
     OpMutable2 t s
-(<\/>) = joinOutInPlaceEff ?joinmeetOutEffort 
+joinOutInPlace = joinOutInPlaceEff ?joinmeetOutEffort 
+
+-- | Outward rounded join assignment 
+(<\/>=) :: 
+    (OuterRoundedLatticeInPlace t, 
+     ?joinmeetOutEffort :: JoinMeetOutEffortIndicator t) => 
+    OpMutable1 t s
+(<\/>=) = mutable2ToMutable1 joinOutInPlace 
 
 -- | Outward rounded in-place meet
-(</\>) :: 
+meetOutInPlace :: 
     (OuterRoundedLatticeInPlace t, 
      ?joinmeetOutEffort :: JoinMeetOutEffortIndicator t) => 
     OpMutable2 t s    
-(</\>) = meetOutInPlaceEff ?joinmeetOutEffort 
+meetOutInPlace = meetOutInPlaceEff ?joinmeetOutEffort 
+
+-- | Outward rounded meet assignment 
+(</\>=) :: 
+    (OuterRoundedLatticeInPlace t, 
+     ?joinmeetOutEffort :: JoinMeetOutEffortIndicator t) => 
+    OpMutable1 t s    
+(</\>=) = mutable2ToMutable1 meetOutInPlace 
 
 -- | Inward rounded in-place join
-(>\/<) :: 
+joinInInPlace :: 
     (InnerRoundedLatticeInPlace t, 
      ?joinmeetInEffort :: JoinMeetInEffortIndicator t) => 
     OpMutable2 t s
-(>\/<) = joinInInPlaceEff ?joinmeetInEffort 
+joinInInPlace = joinInInPlaceEff ?joinmeetInEffort 
+
+-- | Inward rounded join assignment 
+(>\/<=) :: 
+    (InnerRoundedLatticeInPlace t, 
+     ?joinmeetInEffort :: JoinMeetInEffortIndicator t) => 
+    OpMutable1 t s
+(>\/<=) = mutable2ToMutable1 joinInInPlace 
 
 -- | Inward rounded in-place meet
-(>/\<) :: 
+meetInInPlace :: 
     (InnerRoundedLatticeInPlace t, 
      ?joinmeetInEffort :: JoinMeetInEffortIndicator t) => 
     OpMutable2 t s    
-(>/\<) = meetInInPlaceEff ?joinmeetInEffort 
+meetInInPlace = meetInInPlaceEff ?joinmeetInEffort 
 
-{-| convenience Unicode notation for '<\/>' -}
-(<⊔>) :: 
-    (OuterRoundedLatticeInPlace t, ?joinmeetOutEffort :: JoinMeetOutEffortIndicator t) => 
-    OpMutable2 t s
-(<⊔>) = (<\/>)
-{-| convenience Unicode notation for '</\>' -}
-(<⊓>) :: 
-    (OuterRoundedLatticeInPlace t, ?joinmeetOutEffort :: JoinMeetOutEffortIndicator t) => 
-    OpMutable2 t s
-(<⊓>) = (</\>)
+-- | Inward rounded meet assignment 
+(>/\<=) :: 
+    (InnerRoundedLatticeInPlace t, 
+     ?joinmeetInEffort :: JoinMeetInEffortIndicator t) => 
+    OpMutable1 t s    
+(>/\<=) = mutable2ToMutable1 meetInInPlace 
 
-{-| convenience Unicode notation for '>\/<' -}
-(>⊔<) :: 
-    (InnerRoundedLatticeInPlace t, ?joinmeetInEffort :: JoinMeetInEffortIndicator t) => 
-    OpMutable2 t s
-(>⊔<) = (>\/<)
-{-| convenience Unicode notation for '>/\<' -}
-(>⊓<) :: 
-    (InnerRoundedLatticeInPlace t, ?joinmeetInEffort :: JoinMeetInEffortIndicator t) => 
-    OpMutable2 t s
-(>⊓<) = (>/\<)
+{-| convenience Unicode notation for '<\/>=' -}
+(<⊔>=) :: 
+    (OuterRoundedLatticeInPlace t, 
+     ?joinmeetOutEffort :: JoinMeetOutEffortIndicator t) => 
+    OpMutable1 t s
+(<⊔>=) = (<\/>=)
+{-| convenience Unicode notation for '</\>=' -}
+(<⊓>=) :: 
+    (OuterRoundedLatticeInPlace t, 
+     ?joinmeetOutEffort :: JoinMeetOutEffortIndicator t) => 
+    OpMutable1 t s
+(<⊓>=) = (</\>=)
+
+{-| convenience Unicode notation for '>\/<=' -}
+(>⊔<=) :: 
+    (InnerRoundedLatticeInPlace t, 
+     ?joinmeetInEffort :: JoinMeetInEffortIndicator t) => 
+    OpMutable1 t s
+(>⊔<=) = (>\/<=)
+{-| convenience Unicode notation for '>/\<=' -}
+(>⊓<=) :: 
+    (InnerRoundedLatticeInPlace t, 
+     ?joinmeetInEffort :: JoinMeetInEffortIndicator t) => 
+    OpMutable1 t s
+(>⊓<=) = (>/\<=)
