98e7420fca61347b2ed784e4ab7e82d14c81742a
Michal Konečný <mikkonecny@gmail.com>
2011-07-12 12:48:41 +0100

    fixed a bug in C macros for integer multiplication and division

--- AERN-RmToRn-Poly/cbits/GenericCoeff/coeff.h
+++ AERN-RmToRn-Poly/cbits/GenericCoeff/coeff.h
@@ -76,13 +76,13 @@ typedef struct OPS
 #define CFM_MUL_DN(ops,rp,dp1,dp2) \
     (eval_binaryMutable_hs(ops -> zero, ops -> timesDnMutable, rp, dp1, dp2))
 #define CFM_MULI_UP(ops,rp,dp1,n2) \
-    (eval_binaryMixedMutable_hs(ops -> zero, ops -> timesByIntUpMutable, rp, dp1, n2))
+    (eval_mixedIntMutable_hs(ops -> zero, ops -> timesByIntUpMutable, rp, dp1, n2))
 #define CFM_MULI_DN(ops,rp,dp1,n2) \
-    (eval_binaryMixedMutable_hs(ops -> zero, ops -> timesByIntDnMutable, rp, dp1, n2))
+    (eval_mixedIntMutable_hs(ops -> zero, ops -> timesByIntDnMutable, rp, dp1, n2))
 #define CFM_DIVI_UP(ops,rp,dp1,n2) \
-    (eval_binaryMixedMutable_hs(ops -> zero, ops -> divByIntUpMutable, rp, dp1, n2))
+    (eval_mixedIntMutable_hs(ops -> zero, ops -> divByIntUpMutable, rp, dp1, n2))
 #define CFM_DIVI_DN(ops,rp,dp1,n2) \
-    (eval_binaryMixedMutable_hs(ops -> zero, ops -> divByIntDnMutable, rp, dp1, n2))
+    (eval_mixedIntMutable_hs(ops -> zero, ops -> divByIntDnMutable, rp, dp1, n2))
 
 #define CFM_FREE(dp) (free_SP_hs(dp))
 
