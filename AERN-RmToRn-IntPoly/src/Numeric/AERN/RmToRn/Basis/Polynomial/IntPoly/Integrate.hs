ef8aa489ff9e7c1373db0f51323dac3fe5ec4c30
Michal Konečný <mikkonecny@gmail.com>
2012-01-24 17:21:49 +0000

    experimented with generalising IntPoly integrate but commented out all changes

--- AERN-RmToRn-IntPoly/src/Numeric/AERN/RmToRn/Basis/Polynomial/IntPoly/Integrate.hs
+++ AERN-RmToRn-IntPoly/src/Numeric/AERN/RmToRn/Basis/Polynomial/IntPoly/Integrate.hs
@@ -22,6 +22,7 @@ where
     
 import Numeric.AERN.RmToRn.Basis.Polynomial.IntPoly.Basics
 import Numeric.AERN.RmToRn.Basis.Polynomial.IntPoly.RingOps
+import Numeric.AERN.RmToRn.Basis.Polynomial.IntPoly.Substitution
 
 import Numeric.AERN.RmToRn.New
 
@@ -29,23 +30,37 @@ import qualified Numeric.AERN.RealArithmetic.RefinementOrderRounding as ArithInO
 import Numeric.AERN.RealArithmetic.RefinementOrderRounding.OpsImplicitEffort
 import Numeric.AERN.RealArithmetic.ExactOps
 
+--import qualified Numeric.AERN.RefinementOrder as RefOrd
+--import Numeric.AERN.RefinementOrder.OpsImplicitEffort
+
 import qualified Data.IntMap as IntMap
 
 integratePolyMainVar ::
-    (Ord var, Show var, ArithInOut.RoundedReal cf, Show cf) => 
+    (Ord var, Show var, 
+     ArithInOut.RoundedReal cf,
+     Show cf) => 
     (ArithInOut.RoundedRealEffortIndicator cf) ->
     cf {- zero coefficient -} ->
-    IntPoly var cf {- initial value at point 0 -} ->
+    IntPoly var cf {- initial value at 0 -} -> --  point domLE -} ->
     IntPoly var cf {- polynomial to integrate in its main variable -} ->
     IntPoly var cf
-integratePolyMainVar eff z initPoly (IntPoly cfg poly) =
+integratePolyMainVar eff z initPoly p@(IntPoly cfg poly) =
 --    let ?addInOutEffort = effAdd in
     initPoly <+> (IntPoly cfg $ ip poly) 
+--    (initPoly <+> (neg projectionAtDomLE)) <+> (IntPoly cfg $ ip poly) 
     where
     (<+>) = ArithInOut.addOutEff effAdd
     effDiv = ArithInOut.mxfldEffortDiv sample (1::Int) $ ArithInOut.rrEffortIntMixedField sample eff
     effAdd = ArithInOut.fldEffortAdd sample $ ArithInOut.rrEffortField sample eff
     sample = ipolycfg_sample_cf cfg
+    
+--    projectionAtDomLE =
+--        substPolyMainVar eff z domLEPoly p
+--    domLEPoly =
+--        newConstFnFromSample p domLE
+--    (domLE, _) = RefOrd.getEndpointsOutWithDefaultEffort $ head doms
+--    doms = ipolycfg_doms cfg
+    
     ip p@(IntPolyV x polys) =
 --    unsafePrint
 --    (
