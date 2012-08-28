4d3c036cededc114d835ce2ecdb3a7dffa7771a6
mikkonecny <devnull@localhost>
2012-08-28 14:55:02 +0100

    Instances of most function classes for Interval.

--- aern-realfn/src/Numeric/AERN/RmToRn/Interval.hs
+++ aern-realfn/src/Numeric/AERN/RmToRn/Interval.hs
@@ -21,7 +21,9 @@ module Numeric.AERN.RmToRn.Interval where
 import Numeric.AERN.Basics.Interval
 
 import Numeric.AERN.RmToRn.Domain
+import Numeric.AERN.RmToRn.New
 import Numeric.AERN.RmToRn.Evaluation
+import Numeric.AERN.RmToRn.Integration
 
 import qualified Numeric.AERN.RefinementOrder as RefOrd
 --import Numeric.AERN.RefinementOrder.OpsImplicitEffort
@@ -39,6 +41,97 @@ instance
     getDomainBox (Interval l _) = getDomainBox l
     getNSamplesFromDomainBox (Interval l _) = getNSamplesFromDomainBox l
     getSampleFromInsideDomainBox (Interval l _) = getSampleFromInsideDomainBox l
+    
+    
+instance 
+    (HasSizeLimits f)
+    =>
+    (HasSizeLimits (Interval f))
+    where
+    type SizeLimits (Interval f) = SizeLimits f 
+    getSizeLimits (Interval l _) = getSizeLimits l
+    defaultSizeLimits (Interval l _) = defaultSizeLimits l
+    adjustSizeLimitsToVarsAndDombox (Interval l _) vars dombox sizeLims =
+        adjustSizeLimitsToVarsAndDombox l vars dombox sizeLims
+
+instance
+    (CanChangeSizeLimits f,
+     RefOrd.IntervalLike f) 
+    =>
+    (CanChangeSizeLimits (Interval f)) 
+    where
+    type SizeLimitsChangeEffort (Interval f) = 
+        (SizeLimitsChangeEffort f,
+         RefOrd.GetEndpointsEffortIndicator f)
+    sizeLimitsChangeDefaultEffort (Interval l _) = 
+        (sizeLimitsChangeDefaultEffort l,
+         RefOrd.getEndpointsDefaultEffort l)
+    changeSizeLimitsOut (eff, effGetE) sizeLims (Interval l r) = (Interval lN rN)
+        where
+        (lN, _) = 
+            RefOrd.getEndpointsOutEff effGetE $
+                changeSizeLimitsOut eff sizeLims l
+        (_, rN) = 
+            RefOrd.getEndpointsOutEff effGetE $
+                changeSizeLimitsOut eff sizeLims r
+    changeSizeLimitsIn (eff, effGetE) sizeLims (Interval l r) = (Interval lN rN)
+        where
+        (lN, _) = 
+            RefOrd.getEndpointsInEff effGetE $
+                changeSizeLimitsIn eff sizeLims l
+        (_, rN) = 
+            RefOrd.getEndpointsInEff effGetE $
+                changeSizeLimitsIn eff sizeLims r
+
+instance 
+    (HasProjections f,
+     RefOrd.IntervalLike f)
+    => 
+    HasProjections (Interval f)
+    where
+    newProjection sizeLims dombox vars = Interval l r
+        where
+        (l,r) = RefOrd.getEndpointsOutWithDefaultEffort p
+        p = newProjection sizeLims dombox vars
+
+instance
+    (HasConstFns f,
+     RefOrd.IntervalLike f) 
+    => 
+    HasConstFns (Interval f) 
+    where
+    newConstFn sizeLims dombox value = Interval l r
+        where
+        (l,r) = RefOrd.getEndpointsOutWithDefaultEffort p
+        p = newConstFn sizeLims dombox value
+
+instance 
+    CanAddVariables f 
+    => 
+    CanAddVariables (Interval f) 
+    where
+    addVariablesFront varDoms (Interval l r) = Interval lN rN
+        where
+        lN = addVariablesFront varDoms l
+        rN = addVariablesFront varDoms r
+    addVariablesBack varDoms (Interval l r) = Interval lN rN
+        where
+        lN = addVariablesBack varDoms l
+        rN = addVariablesBack varDoms r
+
+instance 
+    CanRenameVariables f
+    => 
+    CanRenameVariables (Interval f) 
+    where
+    renameVar old new (Interval l r) = Interval lN rN
+        where
+        lN = renameVar old new l
+        rN = renameVar old new r
+    renameVars renVar (Interval l r) = Interval lN rN
+        where
+        lN = renameVars renVar l
+        rN = renameVars renVar r
     
 
 instance 
@@ -58,4 +151,29 @@ instance
         where
         lVal = evalAtPointOutEff effEval dombox l
         rVal = evalAtPointOutEff effEval dombox r
-        
+    evalAtPointInEff (effEval, effFromE) dombox (Interval l r) =
+        RefOrd.fromEndpointsInEff effFromE (lVal, rVal)
+        where
+        lVal = evalAtPointInEff effEval dombox l
+        rVal = evalAtPointInEff effEval dombox r
+
+instance 
+    (RoundedIntegration f,
+     RefOrd.IntervalLike f) 
+    => 
+    RoundedIntegration (Interval f) 
+    where
+    type IntegrationEffortIndicator (Interval f) =
+        (IntegrationEffortIndicator f,
+         RefOrd.GetEndpointsEffortIndicator f)
+    integrationDefaultEffort (Interval l _) =
+        (integrationDefaultEffort l,
+         RefOrd.getEndpointsDefaultEffort l)
+    primitiveFunctionOutEff (effInteg, effGetE) (Interval l r) var = Interval lN rN
+        where
+        (lN, _) = RefOrd.getEndpointsOutEff effGetE $ primitiveFunctionOutEff effInteg l var 
+        (_, rN) = RefOrd.getEndpointsOutEff effGetE $ primitiveFunctionOutEff effInteg r var 
+    primitiveFunctionInEff (effInteg, effGetE) (Interval l r) var = Interval lN rN
+        where
+        (lN, _) = RefOrd.getEndpointsInEff effGetE $ primitiveFunctionInEff effInteg l var 
+        (_, rN) = RefOrd.getEndpointsInEff effGetE $ primitiveFunctionInEff effInteg r var 
