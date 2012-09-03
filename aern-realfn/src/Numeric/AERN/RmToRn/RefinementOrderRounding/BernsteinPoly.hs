a95575f90c1943fafff262c2949c6f3244901b89
Michal Konečný <mikkonecny@gmail.com>
2012-09-03 13:08:32 +0100

    fixed integer overflow when computing binomial coefficients during bernstein approximations

--- aern-realfn/src/Numeric/AERN/RmToRn/RefinementOrderRounding/BernsteinPoly.hs
+++ aern-realfn/src/Numeric/AERN/RmToRn/RefinementOrderRounding/BernsteinPoly.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-|
     Module      :  Numeric.AERN.RmToRn.RefinementOrderRounding.BernsteinPoly
     Description :  approximations of Bernstein polynomials  
@@ -15,7 +16,7 @@
 
 module Numeric.AERN.RmToRn.RefinementOrderRounding.BernsteinPoly where
 
---import Numeric.AERN.RmToRn.Domain
+import Numeric.AERN.RmToRn.Domain
 import Numeric.AERN.RmToRn.New
 
 import Numeric.AERN.RealArithmetic.ExactOps
@@ -24,30 +25,72 @@ import Numeric.AERN.RealArithmetic.RefinementOrderRounding.OpsImplicitEffort
 
 import Numeric.AERN.Misc.IntegerArithmetic
 
+import Numeric.AERN.Misc.Debug
+_ = unsafePrint
+
 bernsteinOut ::
     (HasProjections f, HasConstFns f, HasOne f,
      ArithInOut.RoundedRing f,
-     ArithInOut.RoundedMixedField f Int) 
+     ArithInOut.RoundedMixedMultiply f (Domain f),
+     ArithInOut.RoundedReal (Domain f), 
+     Show f, Show (Domain f)) 
      =>
     (ArithInOut.RingOpsEffortIndicator f,
-     ArithInOut.MixedFieldOpsEffortIndicator f Int) ->
+     ArithInOut.RoundedRealEffortIndicator (Domain f),
+     ArithInOut.MixedMultEffortIndicator f (Domain f)) ->
     f ->
     Int ->
     Int ->
     f
-bernsteinOut (effRing, effIntOps) x n p =
-    let ?addInOutEffort = effAdd in
-    let ?multInOutEffort = effMult in
-    let ?mixedMultInOutEffort = effMMult in
-    let ?intPowerInOutEffort = effPow in
-    (binomial n p) |<*>
-        ((x<^>p) <*> ((c1 <-> x)<^>(n-p)))
+bernsteinOut (effRing, effDom, effMMult) x n p =
+--    unsafePrint
+--    (
+--        "bernsteinOut:"
+--        ++ "\n x = " ++ show x
+--        ++ "\n n = " ++ show n
+--        ++ "\n p = " ++ show p
+--        ++ "\n (n p) = " ++ show binomialOut
+--        ++ "\n result = " ++ show result
+--    )
+    result
     where
+    result = 
+        let ?addInOutEffort = effAdd in
+        let ?multInOutEffort = effMult in
+        let ?mixedMultInOutEffort = effMMult in
+        let ?intPowerInOutEffort = effPow in
+        (binomialOut) |<*>
+            ((x<^>p) <*> ((c1 <-> x)<^>(n-p)))
+    binomialOut 
+        | 0 <= p && p <= n =
+            let ?divInOutEffort = effDivD in
+            productNdownP </> factorialP
+        | otherwise = 
+            error $ 
+                "bernsteinOut called with illegal parameters:" 
+                ++ "\n n = " ++ show n 
+                ++ "\n p = " ++ show p 
+        where
+        factorialP = 
+            let ?mixedMultInOutEffort = effMultIntD in
+            foldl (<*>|) (one sampleD) [2..p]
+        productNdownP = 
+            let ?mixedMultInOutEffort = effMultIntD in
+            foldl (<*>|) (one sampleD) [(n-p+1)..n]
+
     sampleF = x
+    sampleD = getSampleDomValue sampleF
     c1 = (one sampleF)
 --    sampleCf = getSampleDomValue sampleF
     effAdd = ArithInOut.ringEffortAdd sampleF effRing
     effMult = ArithInOut.ringEffortMult sampleF effRing
     effPow = ArithInOut.ringEffortPow sampleF effRing
-    effMMult = ArithInOut.mxfldEffortMult sampleF (1::Int) effIntOps
+    effDivD = 
+        ArithInOut.fldEffortDiv sampleD $ 
+            ArithInOut.rrEffortField sampleD effDom
+    effMultIntD = 
+        ArithInOut.mxfldEffortMult sampleD (1::Int) $ 
+            ArithInOut.rrEffortIntMixedField sampleD effDom
+    
+    
     
