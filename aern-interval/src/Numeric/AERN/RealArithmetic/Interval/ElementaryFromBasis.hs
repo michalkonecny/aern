2fc4a3a9e3353e20ff3e7370185e625d3a21813c
Michal Konečný <mikkonecny@gmail.com>
2013-04-10 23:57:18 +0100

    defined alternative Interval instance for elementary operations using a newtype to avoid conflict with the default instance

--- aern-interval/src/Numeric/AERN/RealArithmetic/Interval/ElementaryFromBasis.hs
+++ aern-interval/src/Numeric/AERN/RealArithmetic/Interval/ElementaryFromBasis.hs
@@ -26,32 +26,46 @@ import qualified Numeric.AERN.RealArithmetic.NumericOrderRounding as ArithUpDn
 
 import Numeric.AERN.Basics.Interval
 
+newtype IntervalElementaryFromBasis t = 
+    IntervalElementaryFromBasis { unIntervalElementaryFromBasis :: Interval t }
+
+asIntervalElementaryFromBasis ::
+    ((IntervalElementaryFromBasis t) -> (IntervalElementaryFromBasis t))
+    ->
+    ((Interval t) -> (Interval t))
+asIntervalElementaryFromBasis op interval =
+    unIntervalElementaryFromBasis $ op (IntervalElementaryFromBasis interval)
+
 instance (ArithUpDn.RoundedExponentiationEffort e) => 
-    (ArithInOut.RoundedExponentiationEffort (Interval e)) 
+    (ArithInOut.RoundedExponentiationEffort (IntervalElementaryFromBasis e)) 
     where
-    type ExpEffortIndicator (Interval e) = 
+    type ExpEffortIndicator (IntervalElementaryFromBasis e) = 
         ArithUpDn.ExpEffortIndicator e
-    expDefaultEffort (Interval l r) = ArithUpDn.expDefaultEffort l
+    expDefaultEffort (IntervalElementaryFromBasis (Interval l _)) = ArithUpDn.expDefaultEffort l
 
 instance (ArithUpDn.RoundedExponentiation e) => 
-    (ArithInOut.RoundedExponentiation (Interval e)) 
+    (ArithInOut.RoundedExponentiation (IntervalElementaryFromBasis e)) 
     where
-    expInEff effort (Interval l r) =
-        Interval (ArithUpDn.expUpEff effort l) (ArithUpDn.expDnEff effort r)
-    expOutEff effort (Interval l r) =
-        Interval (ArithUpDn.expDnEff effort l) (ArithUpDn.expUpEff effort r)
+    expInEff effort (IntervalElementaryFromBasis (Interval l r)) =
+        IntervalElementaryFromBasis $
+            Interval (ArithUpDn.expUpEff effort l) (ArithUpDn.expDnEff effort r)
+    expOutEff effort (IntervalElementaryFromBasis (Interval l r)) =
+        IntervalElementaryFromBasis $
+            Interval (ArithUpDn.expDnEff effort l) (ArithUpDn.expUpEff effort r)
 
 instance (ArithUpDn.RoundedSquareRootEffort e) => 
-    (ArithInOut.RoundedSquareRootEffort (Interval e)) 
+    (ArithInOut.RoundedSquareRootEffort (IntervalElementaryFromBasis e)) 
     where
-    type SqrtEffortIndicator (Interval e) = ArithUpDn.SqrtEffortIndicator e
-    sqrtDefaultEffort (Interval l r) = ArithUpDn.sqrtDefaultEffort l
+    type SqrtEffortIndicator (IntervalElementaryFromBasis e) = ArithUpDn.SqrtEffortIndicator e
+    sqrtDefaultEffort (IntervalElementaryFromBasis (Interval l _)) = ArithUpDn.sqrtDefaultEffort l
 
 instance (ArithUpDn.RoundedSquareRoot e) => 
-    (ArithInOut.RoundedSquareRoot (Interval e)) 
+    (ArithInOut.RoundedSquareRoot (IntervalElementaryFromBasis e)) 
     where
-    sqrtInEff effort (Interval l r) =
-        Interval (ArithUpDn.sqrtUpEff effort l) (ArithUpDn.sqrtDnEff effort r)
-    sqrtOutEff effort (Interval l r) =
-        Interval (ArithUpDn.sqrtDnEff effort l) (ArithUpDn.sqrtUpEff effort r)
+    sqrtInEff effort (IntervalElementaryFromBasis (Interval l r)) =
+        IntervalElementaryFromBasis $
+            Interval (ArithUpDn.sqrtUpEff effort l) (ArithUpDn.sqrtDnEff effort r)
+    sqrtOutEff effort (IntervalElementaryFromBasis (Interval l r)) =
+        IntervalElementaryFromBasis $
+            Interval (ArithUpDn.sqrtDnEff effort l) (ArithUpDn.sqrtUpEff effort r)
         
