104409d9ed48561ead22f2b70a1bfc683d2155fc
mikkonecny <devnull@localhost>
2013-07-10 10:44:34 +0100

    cleaned up miniature demo

--- aern-poly-plot-gtk/demos/quickstart.hs
+++ aern-poly-plot-gtk/demos/quickstart.hs
@@ -22,29 +22,33 @@ import Numeric.AERN.RealArithmetic.Interval.Double (DI, Interval)
 import Numeric.AERN.Poly.IntPoly (IntPoly, IntPolySizeLimits(..), defaultIntPolySizeLimits)
 
 -- abstract approximate order operations:
-import qualified Numeric.AERN.NumericOrder as NumOrd
-import Numeric.AERN.NumericOrder.Operators
-import qualified Numeric.AERN.RefinementOrder as RefOrd
-import Numeric.AERN.RefinementOrder.Operators
+--import qualified Numeric.AERN.NumericOrder as NumOrd
+--import Numeric.AERN.NumericOrder.Operators
+--import qualified Numeric.AERN.RefinementOrder as RefOrd
+import Numeric.AERN.RefinementOrder.Operators ((</\>))
 
 -- abstract approximate real arithmetic operations:
-import qualified Numeric.AERN.RealArithmetic.RefinementOrderRounding as ArithInOut
-import Numeric.AERN.RealArithmetic.RefinementOrderRounding.Operators
-import Numeric.AERN.RealArithmetic.ExactOps
+--import qualified Numeric.AERN.RealArithmetic.RefinementOrderRounding as ArithInOut
+--import Numeric.AERN.RealArithmetic.RefinementOrderRounding.Operators
+--import Numeric.AERN.RealArithmetic.ExactOps
 
 -- abstract function processing operations:
-import Numeric.AERN.RmToRn
+import Numeric.AERN.RmToRn (newConstFn, newProjection)
 
 {----- non-AERN imports -----}
 
-import qualified Data.Map as Map
+--import qualified Data.Map as Map
 
 {----- definitions -----}
 
-type Poly = IntPoly String DI 
-    -- polynomials with string variables and Double interval coefficients
 type PI = Interval Poly
     -- polynomial intervals
+
+type Poly = IntPoly V DI 
+    -- polynomials with V variables and Double interval coefficients
+
+type V = String
+    -- variables
 
 main :: IO ()
 main =
@@ -53,20 +57,18 @@ main =
 
 x :: PI
 x = newProjection sizeLimits varDoms "x"
-c0 :: PI
-c0 = newConstFn sizeLimits varDoms 0
+
 c1 :: PI
 c1 = newConstFn sizeLimits varDoms 1
 
-varDoms :: [(Var Poly, DI)]
+varDoms :: [(V, DI)]
 varDoms = zip vars doms
 
-vars :: [Var Poly]
+vars :: [V]
 vars = ["x"]
 
 doms :: [DI]
 doms = [0 </\> 1]
-
 
 sizeLimits :: IntPolySizeLimits DI
 sizeLimits =
