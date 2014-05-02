9841213823b5c2e61d0bfd02494e08655e3f6fc9
Michal Konečný <mikkonecny@gmail.com>
2014-05-02 22:14:35 +0100

    Slight update to the logistic map demo so that it agrees with description on AERN main page.

--- aern-mpfr/demos/LogisticMap.hs
+++ aern-mpfr/demos/LogisticMap.hs
@@ -5,13 +5,13 @@ import qualified Numeric.AERN.MPFRBasis.Interval as MI
 
 -- numerical comparison abstraction and operators:
 import qualified Numeric.AERN.NumericOrder as NumOrd
-import Numeric.AERN.NumericOrder.OpsDefaultEffort
+import Numeric.AERN.NumericOrder.Operators
 
 -- real arithmetic operators and imprecision measure:
 import Numeric.AERN.RealArithmetic.ExactOps
 import Numeric.AERN.RealArithmetic.Measures
 import qualified Numeric.AERN.RealArithmetic.RefinementOrderRounding as ArithInOut
-import Numeric.AERN.RealArithmetic.RefinementOrderRounding.OpsDefaultEffort
+import Numeric.AERN.RealArithmetic.RefinementOrderRounding.Operators
 
 -- generic tools for controlling effort and formatting:
 import Numeric.AERN.Basics.Effort
@@ -28,7 +28,8 @@ main =
     -- print each line asap:
     hSetBuffering stdout LineBuffering
     -- boilerplate to process arguments:
-    [itersS, digitsS] <- getArgs
+    args <- getArgs
+    let [itersS, digitsS] = args 
     let iters = read itersS -- number of iterations of the logistic map
     let digits = read digitsS -- desired accuracy in decimal digits
 
@@ -39,13 +40,13 @@ main =
         -- invoke an iRRAM-style procedure for automatic precision/effort incrementing: 
         iterateUntilAccurate maxIncrements (maxImprecision digits) initPrec $ 
             -- on the computation of iters-many iterations of the logistic map:
-            \prec -> ((logistic prec r x0) !! (iters - 1))
+            \prec -> ((logisticMap prec r x0) !! (iters - 1))
     
     r :: Rational
-    r = 40 / 10
+    r = 375 / 100
     
     x0 :: RealApprox
-    x0 = 0.671875
+    x0 = 0.5 -- 0.671875
 
     maxImprecision :: Int -> RealApprox
     maxImprecision digits = (ensurePrecision 10 10)^^(-digits)
@@ -65,12 +66,12 @@ main =
         shouldShowInternals = (digitsW+2, False)
         digitsW = fromIntegral digits
         
-logistic :: 
+logisticMap :: 
     Precision -> 
     Rational {-^ scaling constant r -} -> 
     RealApprox {-^ initial value x_0 -} -> 
     [RealApprox]  {-^ sequence x_k defined by x_{k+1} = r*x_k*(1 - x_k) -}
-logistic prec r x0 =
+logisticMap prec r x0 =
     iterate logisticAux $ ensurePrecision prec x0
     where
     logisticAux xPrev =
@@ -78,6 +79,6 @@ logistic prec r x0 =
 
 ensurePrecision :: Precision -> RealApprox -> RealApprox
 ensurePrecision prec x =
-    (ArithInOut.convertOutEff prec (0:: Int)) <+> x 
+    (ArithInOut.convertOutEff prec x (0:: Int)) <+> x 
         
              
