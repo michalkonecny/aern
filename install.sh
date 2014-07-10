c86768082c00ea61e63907ed16cd33db86a1a326
Michal Konečný <mikkonecny@gmail.com>
2014-07-10 15:16:30 +0100

    A script to install all the packages in the correct order.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+(cd aern-order; cabal install --force-reinstalls)
+(cd aern-real; cabal install --force-reinstalls)
+(cd aern-interval; cabal install --force-reinstalls)
+(cd aern-double; cabal install --force-reinstalls)
+(cd aern-mpfr; cabal install --force-reinstalls)
+(cd aern-realfn; cabal install --force-reinstalls)
+(cd aern-realfn-plot-gtk; cabal install --force-reinstalls)
+(cd aern-poly; cabal install --force-reinstalls)
+(cd aern-poly-plot-gtk; cabal install --force-reinstalls)
+(cd aern-ivp; cabal install --force-reinstalls)
