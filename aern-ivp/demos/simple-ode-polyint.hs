099fd48205391cd0921742fc03240bb7a110cd59
Michal Konečný <mikkonecny@gmail.com>
2014-07-17 14:01:38 +0100

    Add ability to specify which variables should be scanned when plotting a sample in a parametric plot.  These variables can define a non-rectangular shape.

--- aern-ivp/demos/simple-ode-polyint.hs
+++ aern-ivp/demos/simple-ode-polyint.hs
@@ -114,7 +114,7 @@ runWithArgs [ivpName, tEndS, maybePlotDimensS, maybePDFfilenameS,
         }
     ivp = ivpByNameReportError ivpName sampleFn
     wrapType = read wrapTypeS :: WrapType
-    maybePlotDimens = readIVPPlotArgs maybePlotDimensS :: Maybe IVPPlotArgs
+    maybePlotDimens = readIVPPlotArgs maybePlotDimensS :: Maybe (IVPPlotArgs (Var Fn))
     maybePDFfilename = readPDFfilename maybePDFfilenameS :: Maybe String
     maxDeg = read maxDegS :: Int
     maxSize = read maxSizeS :: Int
@@ -227,7 +227,7 @@ solveVtPrintSteps ::
     => 
     WrapType
     ->
-    (Maybe IVPPlotArgs)
+    (Maybe (IVPPlotArgs (Var Fn)))
     ->
     (Maybe FilePath)
     ->
