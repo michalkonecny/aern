eca7ff564296a3cb18de70cbf7d55f08e5736d2c
Michal Konečný <mikkonecny@gmail.com>
2014-07-20 19:56:10 +0100

    Further readability improvements to the logistic map demo.

--- aern-mpfr/demos/LogisticMap.hs
+++ aern-mpfr/demos/LogisticMap.hs
@@ -5,18 +5,18 @@ import qualified Numeric.AERN.MPFRBasis.Interval as MI
 
 -- real arithmetic operators and imprecision measure:
 import Numeric.AERN.RealArithmetic.RefinementOrderRounding.Operators ((|<*>))
-import Numeric.AERN.RealArithmetic.RefinementOrderRounding (convertOutEff)
+import Numeric.AERN.RealArithmetic.RefinementOrderRounding (RoundedReal)
 import Numeric.AERN.RealArithmetic.Measures (imprecisionOf, iterateUntilAccurate)
 
 -- ability to change precision of numbers:
 import Numeric.AERN.Basics.SizeLimits (changeSizeLimitsOut)
 
--- generic tools for controlling formatting:
+-- generic tools for controlling formatting of variable precision approximations:
 import Numeric.AERN.Basics.ShowInternals (showInternals)
 
 
-import System.IO
-import System.Environment
+import System.IO (stdout, hSetBuffering, BufferMode(LineBuffering))
+import System.Environment (getArgs)
 
 type RealApprox = MI.MI -- MPFR interval
 type Precision = MI.Precision
@@ -31,6 +31,12 @@ processArgs [itersS, digitsS] =
         _ -> error $ usage
 processArgs _ = error $ usage
 
+r :: Rational
+r = 375 / 100
+
+x0 :: Rational
+x0 = 0.5 -- 0.671875
+
 main =
     do
     -- set console to print each line asap:
@@ -40,33 +46,21 @@ main =
     let (iters, digits) = processArgs args 
 
     -- compute and print the result for each precision:
-    mapM_ (reportItem digits) $ items iters digits
+    mapM_ (reportAttempt digits) $ attemptsWithIncreasingPrecision iters digits
     
     where
-    items iters digits =
+    attemptsWithIncreasingPrecision iters digits =
         -- invoke an iRRAM-style procedure for automatic precision/effort incrementing 
         --    on the computation of iters-many iterations of the logistic map:
-        (iterateUntilAccurate initPrec maxAttempts maxImprecision) 
-            logisticMapWithPrec
+        iterateUntilAccurate initPrec maxAttempts maxImprecision $ 
+            \ prec -> logisticMapIterateNTimes r (fromRationalWithPrec prec x0) iters
         where
         initPrec = 50 -- try with the precision first
         maxAttempts = 100 -- try to increase precision 100 times before giving up
-        maxImprecision = (ensurePrecision 1000 10)^^(-digits) -- target result precision
+        maxImprecision = 10^^(-digits) -- target result precision
         
-        logisticMapWithPrec prec = 
-            -- iterate logistic map iters times, using precision prec
-            (iterate (logisticMap r) x0Prec) !! (iters - 1)
-            where
-            x0Prec = ensurePrecision prec x0
-    
-            r :: Rational
-            r = 375 / 100
-            
-            x0 :: RealApprox
-            x0 = 0.5 -- 0.671875
-        
-    
-    reportItem digits (prec, res) =    
+    -- print the result of an attempt, printing sufficiently many digits, according to the digits parameter
+    reportAttempt digits (prec, res) =    
         putStrLn $ formatRes prec res 
         where
         formatRes prec res =
@@ -76,20 +70,36 @@ main =
         shouldShowInternals = (digitsW+2, False)
         digitsW = fromIntegral digits        
        
-
-logisticMap ::
-    Rational {-^ scaling constant r -} -> 
-    RealApprox {-^ previous value x_0 -} ->
-    RealApprox {-^ result -} 
-logisticMap r xPrev =
-    r |<*> (xPrev * (1 - xPrev))
+fromRationalWithPrec :: 
+    Precision -> Rational -> RealApprox
+    
+fromRationalWithPrec prec x =
+    x |<*> (changeSizeLimitsOut prec 1)
     -- The operator |<*> stands for mixed-type outwards-rounded multiplication.
     -- The <> surrounding the operator * indicate outwards rounding.
     -- The | preceding the operator <*> indicate that the type of the first
     -- operand is different than the type of the second operand and the result.
 
-ensurePrecision :: Precision -> RealApprox -> RealApprox
-ensurePrecision prec x =
-    changeSizeLimitsOut prec x
+logisticMapIterateNTimes ::
+    (Num real, RoundedReal real)
+    =>
+    Rational {-^ @r@ scaling constant -} -> 
+    real {-^ @x0@ initial value  -} ->
+    Int {-^ @n@ number of iterations -} ->
+    real {-^ @logisticMap^n(x0)@ -} 
+
+logisticMapIterateNTimes r x0 n = 
+    (iterate (logisticMap r) x0) !! n
+    
+
+logisticMap ::
+    (Num real, RoundedReal real)
+    =>
+    Rational {-^ scaling constant r -} -> 
+    real {-^ previous value x -} ->
+    real {-^ rx(1-x) -} 
+
+logisticMap r xPrev =
+    r |<*> (xPrev * (1 - xPrev))
         
              
