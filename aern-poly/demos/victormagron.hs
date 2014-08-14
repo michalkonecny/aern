315efcb4beaa4bf49c32a0db4d30c6c740a13bd4
Michal Konečný <mikkonecny@gmail.com>
2014-08-14 17:36:44 +0100

    Fixed bracketing bug in Flyspeck expression and updated the results.

--- aern-poly/demos/victormagron.hs
+++ aern-poly/demos/victormagron.hs
@@ -8,6 +8,8 @@ import Numeric.AERN.RealArithmetic.Basis.Double ()
 --import Numeric.AERN.RealArithmetic.Basis.MPFR
 import Numeric.AERN.Basics.Interval
 
+import Numeric.AERN.RealArithmetic.NumericOrderRounding.ElementaryFromFieldOps.Sqrt (SqrtThinEffortIndicator(..))
+
 import qualified Numeric.AERN.RealArithmetic.RefinementOrderRounding as ArithInOut
 import Numeric.AERN.RealArithmetic.RefinementOrderRounding.Operators
 
@@ -42,44 +44,46 @@ main =
 
 {------
 
-(maxdeg, maxsize, recipdeg, evalsplitsize) -> range, time 
+Results for various choices of params: 
 
-(10, 100, 8, 100) -> [_-972793.072247958,7683443.423662806^], 5.4s
-(10, 100, 8, 1000) -> [_-972793.072247958,7683408.438461192^], 47s
-(10, 200, 8, 100) -> [_-87430.5804374732,6873411.247332599^], 11.8s
-(10, 300, 8, 100) -> [_-18708.75565867797,6600028.754532665^], 16.7s
-(10, 400, 8, 100) ->  [_24754.284036349527,6281330.040812409^], 20.88s
-(10, 500, 8, 100) ->  [_38138.95909293841,6103350.005896601^], 22.5s
-(10, 700, 8, 100) -> [_71173.31500771112,5897926.955546107^], 31.3s
-* (10, 800, 8, 100) -> [_73288.21702714099,5827688.490139663^], 40.8s 
-(10, 1000, 8, 100) -> [_73288.21702714099,5738954.230274559^], 47s
-(10, 1000, 8, 1000) -> [_73288.21702714099,5698260.713513501^], 358s
-(10, 1200, 8, 100) -> [_73288.21702714099,5703914.65761335^], 59.1s
-(10, 1000, 10, 100) -> [_73288.21702714099,5738954.230274559^], 51.5s
-(12, 1000, 8, 100) -> [_73288.21702714099,5738954.230274559^], 48.8s 
+"(6,20,1,100) -> [_0.15211827195519825,2.21712006899124^]"
+(0.79 secs, 733925428 bytes)
 
+"(10,100,1,100) -> [_0.18368014851264433,2.112064768342252^]"
+(3.84 secs, 3732886624 bytes)
 
-(20, 1000, 8, 1000) -> [_73288.21702714099,5698260.713513501^], 359s 
-(20, 2000, 10, 1000) -> [_73288.21702714099,5648992.123038283^], 436s
-
-sqrt(73288.21702714099) > 270.7179658374 
+"(10,1000,1,200) -> [_0.18368029513801096,2.112064768342252^]"
+(6.76 secs, 6307827848 bytes)
 
 ------}
 
-(maxdeg, maxsize, recipdeg, evalsplitsize) = (10, 800, 8, 100) 
+params@(maxdeg, maxsize, recipdeg, evalsplitsize) = (6, 20, 1, 100) 
 
 result =
-    show (maxdeg, maxsize, recipdeg, evalsplitsize) 
-    ++ " -> " ++ show range_flyspeckSqr
+    show params 
+    ++ " -> " ++ show r_flyspeckXXX
 
---f_flyspeck =
---    f_delta4DeltaX / (ArithInOut.sqrtOutEff effSqrt $ 4 * x1 * f_DeltaX) 
 
-range_flyspeckSqr =
-    (evalAtPointOut (getDomainBox x1) f_flyspeckSqr)
+{- 
+    For x=x1=x2=...=x6 we get:
+    
+     f_delta4DeltaX = 2x^2
+     
+     f_DeltaX = 2x^3
+     
+     (r_flyspeckXXX)^2 = 4x^4 / 8 x^4 = 0.5
+-}
 
-f_flyspeckSqr =
-    (f_delta4DeltaX * f_delta4DeltaX) / 4 * x1 * f_DeltaX
+r_flyspeckXXX = r_delta4DeltaX / (ArithInOut.sqrtOutEff effCfSqrt r_4x1DeltaX)
+
+r_4x1DeltaX = rangeOut f_4x1DeltaX
+
+f_4x1DeltaX = (4::Int) |* (x1 * f_DeltaX)
+
+r_delta4DeltaX = rangeOut f_delta4DeltaX
+
+rangeOut fn =
+    evalAtPointOut (getDomainBox fn) fn
 
 f_delta4DeltaX = 
     x1 * (-x1 + x2 + x3 - x4 + x5 + x6) + x2 * x5 + x3 * x6 - x2 * x3 - x5 * x6
@@ -125,7 +129,12 @@ effIP =
 effCf :: ArithInOut.RoundedRealEffortIndicator CF
 --effCf = (100, (100,())) -- MPFR with explicit precision
 effCf = ArithInOut.roundedRealDefaultEffort (0:: CF)
-    
+
+effCfSqrt = (ArithInOut.sqrtDefaultEffort (0 :: CF))
+    {
+        sqrteff_newtonIters = 14
+    }
+
 effIPsplitSize :: Int -> IntPolyEffort CF
 effIPsplitSize maxsplitSize =
     effIP { ipolyeff_evalMaxSplitSize = maxsplitSize }
@@ -136,8 +145,8 @@ minmaxUpDnEff = effIP { ipolyeff_minmaxBernsteinDegree = 10 }
 minmaxInOutEff :: IntPolyEffort CF
 minmaxInOutEff = effIP { ipolyeff_minmaxBernsteinDegree = 10 }
 
-evalOpsOutCf :: PolyEvalOps String CF CF
-evalOpsOutCf = evalOpsEff effIP x1 (0::CF)
+--evalOpsOutCf :: PolyEvalOps String CF CF
+--evalOpsOutCf = evalOpsEff effIP x1 (0::CF)
 
 limits :: IntPolySizeLimits CF
 limits =
@@ -150,12 +159,19 @@ limits =
     } 
 
 varDoms :: [(Var Poly, CF)]
-varDoms = 
-    take 6 $ zip vars $ repeat dom 
+varDoms =
+    [
+        ("x1", 4 </\> (6.3504)),
+        ("x2", 4 </\> (6.3504)),
+        ("x3", 4 </\> (6.3504)),
+        ("x4", 4 </\> (6.3504)),
+        ("x5", 4 </\> (6.3504)),
+        ("x6", 4 </\> (6.3504))
+    ] 
+--    take 6 $ zip vars $ repeat dom 
 
-
-vars :: [Var Poly]
-vars = map (\n -> "x" ++ show n) [1..]
-
-dom = 4 </\> (6.3504)
+--vars :: [Var Poly]
+--vars = map (\n -> "x" ++ show n) [1..]
+--
+--dom = 4 </\> (6.3504)
 
